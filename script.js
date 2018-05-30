6ff738c11259708386bf020a2fde939576f983ff
martinhilary <martinhilary96@gmail.com>
2018-05-31 00:07:56 +0800

    4th Part

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
-var temperature= prompt("Enter a starting temperature");
+var STARTING_TEMP= prompt("Enter a starting temperature");
 var temperatureUnit = prompt("Enter the temperature unit","Either in K, F or Celcius");
+var STARTING_TEMP_1= parseInt(STARTING_TEMP);
 if(temperatureUnit == "K")
 {
-	var temperatureInFarenheit =(temperature*9/5)-459.67;
-	var temperatureInCelcius = temperature- 273.15;
-	var temperatureInKelvin = temperature;
+	var temperatureInFarenheit =(STARTING_TEMP_1*9/5)-459.67;
+	var temperatureInCelcius = STARTING_TEMP_1- 273.15;
+	STARTING_TEMP= STARTING_TEMP_1 + "Kelvin "
+	CONVERTED_TEMP_1= temperatureInFarenheit +" Farenheit ";
+	CONVERTED_TEMP_2=temperatureInCelcius+" Celcius";
+	var temps = [ STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2 ];
+	for (i=0; i< temps.length; i++)
+	{
+	temps[i];
+	console.log(temps[i])
+	}
 }
 
 else if (temperatureUnit == "F")
 {
-	var temperatureInCelcius =(temperature-32)*5/9;
-	var temperatureInKelvin =(temperature+459.67)*5/9;
-	var temperatureInFarenheit=temperature;
+	var temperatureInCelcius =(STARTING_TEMP_1-32)*5/9;
+	var temperatureInKelvin =(STARTING_TEMP_1+459.67)*5/9;
+	STARTING_TEMP= STARTING_TEMP_1 + "Farenheight ";
+	CONVERTED_TEMP_1= temperatureInKelvin+ " kelvin ";
+	CONVERTED_TEMP_2=temperatureInCelcius+ " Celcius";
+	var temps = [ STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2 ];
+	for (i=0; i< temps.length; i++)
+	{
+	temps[i];
+	console.log(temps[i])
+	}	
 }
 
 else
 {
-var temperatureInKelvin= temperature+273.15;
-var temperatureInFarenheit= ((temperature*1.8)+32);
-var temperatureInCelcius= temperature;
+var temperatureInKelvin= STARTING_TEMP_1+273.15;
+var temperatureInFarenheit= ((STARTING_TEMP_1*1.8)+32);
+STARTING_TEMP= STARTING_TEMP_1 + " Celcius ";
+CONVERTED_TEMP_1= temperatureInKelvin+ " kelvin ";
+CONVERTED_TEMP_2=temperatureInFarenheit+ " Farenheit";
+var temps = [ STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2 ];
+for (i=0; i< temps.length; i++)
+	{
+	temps[i];
+	console.log(temps[i])
+	}
 }
-
-console.log(temperatureInCelcius + " degrees")
-console.log(temperatureInKelvin + " kelvin")
-console.log(temperatureInFarenheit + " farenheit")
 
