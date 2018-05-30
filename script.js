159192dcd5fea542da1bc92f85901aea9b593fcb
delontoh <delontoh89@gmail.com>
2018-05-30 21:56:09 +0800

    Part 2

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-// prompt temp in F
-var tempInFar = prompt("Temperature in Fahrenheit");
-//convert temp to C
-var tempInCel = (tempInFar - 32) * (5 / 9)
-console.log(tempInCel + " C");
-//convert temp to K
-var tempInKel = (tempInFar + 459.67) * (5 / 9)
-console.log(tempInKel + " K");
+// prompt temp 
+var temp = parseInt(prompt("Input temperature"), 10);
 
+// prompt unit
+var tempUnit = prompt("Specify Unit eg. C , K or F")
+
+
+// converting Celsius
+if (tempUnit == "C") {
+	var tempInFar = (temp * (9 / 5) + 32);  // to Fahrenheit
+	var tempInKel = (temp + 273.15); // to Kelvin
+	var result = temp + " " + tempUnit + " = " + tempInFar + " F" + " = " + tempInKel + " K"
+	console.log(result);
+	alert(result);
+}  
+// converting Kelvin
+else if (tempUnit == "K") {
+	var tempInFar = (temp * (9 / 5) - 459.67);
+	var tempInCel = (temp - 273.15)
+	var result = temp + " " + tempUnit + " = " + tempInFar + " F" + " = " + tempInCel + " C"
+	console.log(result);
+	alert(result);
+}
+// converting Fahrenheit
+else {
+	var tempInCel = (temp - 32 * (5 / 9))
+	var tempInKel = (temp + 459.67 * (5 / 9))
+	var result = temp + " " + tempUnit + " = " + tempInCel + " C" + " = " + tempInKel + " K"
+	console.log(result);
+	alert(result);
+};
