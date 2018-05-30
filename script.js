ee1d8985412c0671937c04515633498700db0f07
martinhilary <martinhilary96@gmail.com>
2018-05-30 23:59:30 +0800

    3rd part

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-var temperature= prompt("Enter a starting temperature");
+var STARTING_TEMP= prompt("Enter a starting temperature");
 var temperatureUnit = prompt("Enter the temperature unit","Either in K, F or Celcius");
+parseInt(STARTING_TEMP);
 if(temperatureUnit == "K")
 {
-	var temperatureInFarenheit =(temperature*9/5)-459.67;
-	var temperatureInCelcius = temperature- 273.15;
-	var temperatureInKelvin = temperature;
+	parseInt(STARTING_TEMP);
+	var temperatureInFarenheit =(STARTING_TEMP*9/5)-459.67;
+	var temperatureInCelcius = STARTING_TEMP- 273.15;
+	CONVERTED_TEMP_1= temperatureInFarenheit;
+	CONVERTED_TEMP_2=temperatureInCelcius;
+	var temps = [ STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2 ];
+	console.log(temps[0]+" "+temperatureUnit+" "+ temps[1]+" Farenheit "+ temps[2]+ " Celcius")
 }
 
 else if (temperatureUnit == "F")
 {
-	var temperatureInCelcius =(temperature-32)*5/9;
-	var temperatureInKelvin =(temperature+459.67)*5/9;
-	var temperatureInFarenheit=temperature;
+	parseInt(STARTING_TEMP);
+	var temperatureInCelcius =(STARTING_TEMP-32)*5/9;
+	var temperatureInKelvin =(STARTING_TEMP+459.67)*5/9;
+	CONVERTED_TEMP_1= temperatureInKelvin;
+	CONVERTED_TEMP_2=temperatureInCelcius;
+	var temps = [ STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2 ];
+	console.log(temps[0]+" "+temperatureUnit+" "+ temps[1]+
+		 " kelvin"+ temps[2]+ " Celcius")
+
 }
 
 else
 {
-var temperatureInKelvin= temperature+273.15;
-var temperatureInFarenheit= ((temperature*1.8)+32);
-var temperatureInCelcius= temperature;
+parseInt(STARTING_TEMP);
+var temperatureInKelvin= STARTING_TEMP+273.15;
+var temperatureInFarenheit= ((STARTING_TEMP*1.8)+32);
+CONVERTED_TEMP_1= temperatureInKelvin;
+CONVERTED_TEMP_2=temperatureInFarenheit;
+var temps = [ STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2 ];
+console.log(temps[0]+" "+temperatureUnit+" "+ temps[1]+
+		 " kelvin "+ temps[2]+ " Farenheit")
 }
-
-console.log(temperatureInCelcius + " degrees")
-console.log(temperatureInKelvin + " kelvin")
-console.log(temperatureInFarenheit + " farenheit")
 
