22f19b816e6c4c19be5ea7588982531b1226c7be
jodich <jodi_choo@hotmail.com>
2018-05-30 18:45:10 +0800

    Part Two

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var temperatureInFahr = prompt("What is the temperature? (in Fahrenheit)")
 var temperatureInCelsius = (temperatureInFahr - 32) * (5/9) 
 var temperatureInKelvin = ((temperatureInFahr - 32) * 5/9) + 273.15
-console.log(temperatureInCelsius);
-console.log(temperatureInKelvin);
+console.log(temperatureInCelsius + "C");
+console.log(temperatureInKelvin) + "K";
