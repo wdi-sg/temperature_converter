6fe6f96cddca59fd961a7587aca2da88ef07b635
tingsoon <angts8@hotmail.com>
2018-05-30 20:24:28 +0800

    Updated part 2

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ var temperature = prompt("What is the temperature??");
 
 var temperatureUnit = prompt("What is the temperature Unit??");
 
-var temperature = parseInt(temperature)
+var temperature = parseInt(temperature);
 
-if (temperatureUnit = "Fahrenheit") {
+if (temperatureUnit == "Fahrenheit") {
 	temperatureFahr = temperature;
 	temperatureCels = temperature - 32;
 	temperatureKelv = temperature + 241.15;
-} else if (temperatureUnit = "Celsius") {
+	console.log (temperatureFahr + "\xB0 F = " + temperatureCels + "\xB0C = " + temperatureKelv + "K");
+} else if (temperatureUnit == "Celsius") {
 	temperatureCels = temperature;
 	temperatureFahr = temperature + 32;
 	temperatureKelv = temperature + 273.15;
-} else if (temperatureUnit = "Kelvin") {
+	console.log (temperatureFahr + "\xB0 F = " + temperatureCels + "\xB0C = " + temperatureKelv + "K");
+} else if (temperatureUnit == "Kelvin") {
 	temperatureKelv = temperature;
 	temperatureCels = temperature - 273.15;
-	temperatureKelv = temperature - 241.15;
+	temperatureFahr = temperature - 241.15;
+	console.log (temperatureFahr + "\xB0 F = " + temperatureCels + "\xB0C = " + temperatureKelv + "K");
 } else {
 	console.log ("No such Unit!!");
 }
 
-console.log (temperatureFahr + "\xB0 F = " + temperatureCels + "\xB0C = " + temperatureKelv + "K")
