0b11478245f348ad389a5445d2a7c859d69dfa7f
delontoh <delontoh89@gmail.com>
2018-05-30 22:49:01 +0800

    Part 3

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 // prompt temp 
-var temp = parseInt(prompt("Input temperature"), 10);
+var STARTING_TEMP = parseInt(prompt("Input temperature"), 10);
 
 // prompt unit
-var tempUnit = prompt("Specify Unit eg. C , K or F")
+var tempUnit = prompt("Specify Unit eg. C , K or F");
 
+var temps = [];
 
 // converting Celsius
 if (tempUnit == "C") {
-	var tempInFar = (temp * (9 / 5) + 32);  // to Fahrenheit
-	var tempInKel = (temp + 273.15); // to Kelvin
-	var result = temp + " " + tempUnit + " = " + tempInFar + " F" + " = " + tempInKel + " K"
-	console.log(result);
-	alert(result);
+	var CONVERTED_TEMP_1 = (STARTING_TEMP * (9 / 5) + 32) + " F"; 	// to Fahrenheit
+	var CONVERTED_TEMP_2 = (STARTING_TEMP + 273.15) + " K"; 	// to Kelvin
+	temps.push(STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2);
+	console.log(temps);
+	alert(temps);
 }  
 // converting Kelvin
 else if (tempUnit == "K") {
-	var tempInFar = (temp * (9 / 5) - 459.67);
-	var tempInCel = (temp - 273.15)
-	var result = temp + " " + tempUnit + " = " + tempInFar + " F" + " = " + tempInCel + " C"
-	console.log(result);
-	alert(result);
+	var CONVERTED_TEMP_1 = (STARTING_TEMP * (9 / 5) - 459.67) + " F";
+	var CONVERTED_TEMP_2 = (STARTING_TEMP - 273.15) + " C";
+	temps.push(STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2);
+	console.log(temps);
+	alert(temps);
 }
 // converting Fahrenheit
 else {
-	var tempInCel = (temp - 32 * (5 / 9))
-	var tempInKel = (temp + 459.67 * (5 / 9))
-	var result = temp + " " + tempUnit + " = " + tempInCel + " C" + " = " + tempInKel + " K"
-	console.log(result);
-	alert(result);
+	var CONVERTED_TEMP_1 = (STARTING_TEMP - 32 * (5 / 9)) + " C";
+	var CONVERTED_TEMP_2 = (STARTING_TEMP + 459.67 * (5 / 9)) + "K";
+	temps.push(STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2);
+	console.log(temps);
+	alert(temps);
 };
+
