10de63024f4b0afcb5d72ac06eab046d730cee3b
saywandeusher <ongsaywan@hotmail.com>
2018-05-31 00:24:35 +0800

    Part 3

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(celsius.toFixed(3))
 var kelvin = ((temperatureInFahr -32) * (5/9) + 273.15)
 console.log(kelvin.toFixed(3))
 
-var input2 = prompt(" Key in the value of Temperature(Eg.28)");
+var STARTING_TEMP = prompt(" Key in the value of Temperature number");
 var input3 = prompt(" Key in the Temperature Unit (fahr,celsius or kelvin.)");
 function FahrToCelsius(x){
 	return (x - 32) * (5/9)
@@ -26,14 +26,14 @@ function KelvinToCelsius(x){
 	return x - 273.15
 }
 if (input3 === "fahr"){
-	var x = parseInt(input2)
-	var results = console.log(x + "F = " + FahrToCelsius(x).toFixed(3) + "C = " + FahrToKelvin(x).toFixed(3) + "K")
+	var temps = [parseInt(STARTING_TEMP), FahrToCelsius(parseInt(STARTING_TEMP)).toFixed(3), FahrToKelvin(parseInt(STARTING_TEMP)).toFixed(3)]
+	console.log(temps[0] + "F = " + temps[1] + "C = " + temps[2] + "K")
 }else if(input3 === "celsius"){
-	var x = parseInt(input2)
-	var results = console.log(CelsiusToFahr(x) + "F = " + x + "C = " + CelsiusToKelvin(x).toFixed(3) + "K")
+	var temps = [parseInt(STARTING_TEMP), CelsiusToFahr(parseInt(STARTING_TEMP)).toFixed(3), CelsiusToKelvin(parseInt(parseInt(STARTING_TEMP))).toFixed(3)]
+	console.log(temps[0] + "C = " + temps[1] + "F = " + temps[2] + "K")
 }else if(input3 === "kelvin"){
-	var x = parseInt(input2)
-	var results = console.log(KelvinToFahr(x).toFixed(3) + "F = " + KelvinToCelsius(x).toFixed(3) + "C = " + x + "K")
+	var temps = [parseInt(STARTING_TEMP), KelvinToFahr(parseInt(STARTING_TEMP)).toFixed(3), KelvinToCelsius(parseInt(STARTING_TEMP)).toFixed(3)]
+	console.log(temps[0] + "K = " + temps[1] + "F = " + temps[2] + "C")
 }else{
 	console.log("Invalid input. Please try again.")
 }
