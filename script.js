bf7092da6ca742b8cf88719581d96dcbb9581e2b
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-05-30 20:19:01 +0800

    part 3 tested

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ var temperatureInKelvin;
 var temperatureInCelcius;
 var temperatureInFahr;
 
-
 var STARTING_TEMP = prompt("What is the temperature?");
 STARTING_TEMP = parseInt(STARTING_TEMP)
 var temperatureUnit = prompt("What is the temperature Unit?", "F, Deg C or K") 
  	
  	
-	if (temperatureUnit !== 'F' && temperatureUnit !==  'Deg C' && temperatureUnit !==  'K') {
+	if (temperatureUnit != 'F' && temperatureUnit !=  'Deg C' && temperatureUnit !=  'K') {
  		prompt("Answer using the following Inputs ", "F, Deg C or K")
  	}
 
@@ -21,7 +20,7 @@ var temperatureUnit = prompt("What is the temperature Unit?", "F, Deg C or K")
  			CONVERTED_TEMP_2 = temperatureInCelcius;
  	}
 
- 	 if (temperatureUnit == "Deg C") {
+ 	if (temperatureUnit == "Deg C") {
  		temperatureInKelvin = STARTING_TEMP + 273.15 + "K" ;
  			console.log(temperatureInKelvin);
  		temperatureInFahr =  (STARTING_TEMP * 1.8) + 32  + "F" ;
@@ -30,7 +29,7 @@ var temperatureUnit = prompt("What is the temperature Unit?", "F, Deg C or K")
  			CONVERTED_TEMP_2 = temperatureInFahr;
  	}
 
- 	 if (temperatureUnit == "K") {
+ 	if (temperatureUnit == "K") {
  		temperatureInCelcius = STARTING_TEMP - 273.15 + " Deg C" ;
  			console.log(temperatureInCelcius);
  		temperatureInFahr =  (STARTING_TEMP * 1.8) - 459.67  + "F" ;
@@ -39,8 +38,7 @@ var temperatureUnit = prompt("What is the temperature Unit?", "F, Deg C or K")
  			CONVERTED_TEMP_2 = temperatureInFahr;
  	}
 
+
 var temps = [ STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2 ]
-
-
-
+console.log(temps);
 
