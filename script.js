46a6c39a0c40f7b7e324d783cc24c40b98aa1a18
Julian Poh <julianpoh01@gmail.com>
2018-05-30 21:00:18 +0800

    changed JS to part 2

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 //prompt user for starting temperature in fahrenheit and save as variable
-var temperatureInFahr = prompt("What is the temperature (Fahrenheit)?");
+//Changed to starting temperature (numerical value only) 
+var temp = prompt("Please enter temperature.");
+var tempUnit = prompt("Please state temperature unit. (C=Celcius/F=Fahrenheit/K=Kelvin)")
+var tempFahr; 
+var tempKelvin; 
+var tempCelcius; 
 
-//Convert temperature to Celcius and Kelvin
-var temperatureInCelcius = Math.round((temperatureInFahr -32) * (5/9));
-var temperatureInKelvin = Math.round(temperatureInCelcius + 273.15);
 
-//Log data
-console.log(temperatureInFahr + " DegF = " + temperatureInCelcius + " DegC = " + temperatureInKelvin +" K");
+if (tempUnit === "C") {
+    tempFahr = Math.round((temp * 1.8) * 32);
+    tempKelvin = Math.round(temp + 273.15);
+    console.log(temp + tempUnit + " = " + tempFahr + "F = " + tempKelvin + "K");
+} 
+else if (tempUnit === "F") {
+    tempCelcius = Math.round((temp - 32) / 1.8);
+    tempKelvin = Math.round((temp + 459.67) * (5/9));
+    console.log(temp + tempUnit + " = " + tempCelcius + "C = " + tempKelvin + "K");
+}  
+else { 
+    tempCelcius = Math.round(temp - 273.15);
+    tempFahr = Math.round((temp * (9/5))-459.67);
+    console.log(temp + tempUnit + " = " + tempCelcius + "C = " + tempFahr + "F");
+}
 
 
 
