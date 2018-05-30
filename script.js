cac95af58d5cbb040a1ca0615c7401cb6184f4ef
martinhilary <martinhilary96@gmail.com>
2018-05-30 19:19:13 +0800

    part-2

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-var temperatureInFahr=prompt("Please enter a starting temperature.","In Farenheit");
-alert(temperatureInFahr);
-parseInt(temperatureInFahr);
+var temperature= prompt("Enter a starting temperature");
+var temperatureUnit = prompt("Enter the temperature unit","Either in K, F or Celcius");
+if(temperatureUnit == "K")
+{
+	var temperatureInFarenheit =(temperature*9/5)-459.67;
+	var temperatureInCelcius = temperature- 273.15;
+	var temperatureInKelvin = temperature;
+}
 
-var temperatureInDegrees =(temperatureInFahr-32)*5/9;
-var temperatureInKelvin =(temperatureInFahr+459.67)*5/9;
-alert(temperatureInDegrees + " degrees");
-alert(temperatureInKelvin+" kelvin");
+else if (temperatureUnit == "F")
+{
+	var temperatureInCelcius =(temperature-32)*5/9;
+	var temperatureInKelvin =(temperature+459.67)*5/9;
+	var temperatureInFarenheit=temperature;
+}
+
+else
+{
+var temperatureInKelvin= temperature+273.15;
+var temperatureInFarenheit= ((temperature*1.8)+32);
+var temperatureInCelcius= temperature;
+}
+
+console.log(temperatureInCelcius + " degrees")
+console.log(temperatureInKelvin + " kelvin")
+console.log(temperatureInFarenheit + " farenheit")
+
