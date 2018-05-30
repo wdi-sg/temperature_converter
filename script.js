8f89ca433a4154af1fba16752daff701e471df91
jellythewobbly <jellythewobbly@hotmail.com>
2018-05-30 23:20:51 +0800

    Part 1 done

---
+++ script.js
@@ -0,0 +1,22 @@
+var temperatureInFahr = prompt("Please enter a temperature in Fahrenheit: ");
+
+if(temperatureInFahr == NaN){
+	alert("Please enter a temperature in Fahrenheit");
+} else {
+	document.getElementById("Fahrenheit").innerHTML = "You entered " + temperatureInFahr + "°F";
+}
+var deg = (temperatureInFahr - 32) * 5/9;
+var deg2sf = Math.round(deg * 100) / 100
+var kel = (temperatureInFahr - 32) * 5/9 + 273.15;
+var kel2sf = Math.round(kel * 100) / 100
+
+
+document.getElementById("Celcius").innerHTML = "The temperature in Degrees is " + deg2sf + "°C";
+document.getElementById("Kelvin").innerHTML = "The temperature in Kelvin is " + kel2sf + "K";
+alert("The temperature in Degrees is " + deg2sf + "°C");
+alert("The temperature in Kelvin is " + kel2sf + "K");
+
+
+
+console.log("The temperature in Degrees is " + deg2sf + "°C");
+console.log("The temperature in Kelvin is " + kel2sf + "K");
