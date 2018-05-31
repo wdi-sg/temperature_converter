d80a83bcb6829ec8fa7798db4a8c2184997282d1
hygro5d <faidhiamran@Faidhis-MacBook-Pro.local>
2018-05-31 12:18:16 +0800

    some changes

---
+++ script.js
@@ -0,0 +1,15 @@
+//part 1
+
+var temperatureInFahr = prompt("What is the temperature? (in Fahrenheit)")
+var temperatureInCelsius = (temperatureInFahr - 32) * 5 / 9;
+var temperatureInKelvin = (temperatureInFahr + 459.67) * 5 / 9;
+
+
+//part 2
+
+var fahrenheit = "F";
+var celcius = "C";
+var kelvin = "K";
+
+var temperature = prompt('Enter temperature');
+var temperatureUnit = prompt('Enter unit of temperature');
