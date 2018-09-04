d57115a7a26078b8496379eaba927be14ab1f14b
ah5ive <ah5ive@gmail.com>
2018-09-04 20:00:41 +0800

    part 1 of the script

---
+++ script.js
@@ -0,0 +1,6 @@
+var temperatureInFahr = prompt ("What is your temperature now?");
+var Celsius = 5/9 * (temperatureInFahr - 32);
+var Kelvin = 5/9 * (temperatureInFahr - 32) + 273;
+
+console.log(Celsius + " " + "Celsius");
+console.log(Kelvin + " " + "Kelvin");
