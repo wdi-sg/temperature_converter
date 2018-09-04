23a30dba75896a4c6c6db3d2a5350123cb3b0da9
saufm68 <saufm68@gmail.com>
2018-09-04 22:56:08 +0800

    Parts 2 - 5

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
-//Part 1
+//Part 2, 3, 4, 5
+var clothes = [
+ 'nothing',
+ 'swimsuit',
+ 'shorts and shirt',
+ 'sweater',
+ 'sweater + jacket',
+ 'heavy jacket',
+ 'heavy jacket and toe warmers',
+ 'dont go outside if you want to live'
+];
 
-var temperatureInFahr = parseInt(prompt("Enter a temperature in degrees Fahrenheit"));
+var temps = [];
+var celsiusTemp;
 
-while ( isNaN(temperatureInFahr) ) {
+var name = prompt("Enter your name:");
+var temperature = parseInt(prompt("Enter a value for the temperature"));
 
-    var temperatureInFahr = prompt("Please enter a number");
+while ( isNaN(temperature) ) {
+
+    var temperature = parseInt(prompt("Please enter a number")) ;
 
 }
 
-temperatureInKelvin = (5/9) *(temperatureInFahr - 32) + 273;
-temperatureInCelsius = (5/9) * (temperatureInFahr - 32);
+temps.push(temperature);
 
-console.log(`The temperature that you enterd in Kelvin is: ${temperatureInKelvin}.`);
-console.log(`The temperature that you enterd in Celsius is: ${temperatureInCelsius}.`);
+var temperatureUnit = prompt("Please pick one of the 3 units: Fahrenheit/Celsius/Kelvin");
 
+if (temperatureUnit === "Fahrenheit") {
+    temperatureFromFToK = (5/9) *(temperature - 32) +273;
+    temps.push(temperatureFromFToK);
+    temperatureFromFToC = (5/9) * (temperature - 32);
+    temps.push(temperatureFromFToC);
+    celsiusTemp = temperatureFromFToC;
+    console.log (`${temps[0]}°F = ${temps[2]}°C = ${temps[1]}K `);
+}
+
+else if (temperatureUnit === "Celsius") {
+    celsiusTemp = temperature;
+    temperatureFromCToK = temperature + 273;
+    temps.push(temperatureFromCToK);
+    temperatureFromCToF = (9/5)*temperature + 32;
+    temps.push(temperatureFromCToF);
+    console.log (`${temps[0]}°C = ${temps[2]}°F = ${temps[1]}K `);
+}
+
+else if (temperatureUnit === "Kelvin") {
+    temperatureFromKToC = temperature - 273;
+    temps.push(temperatureFromKToC);
+    celsiusTemp = temperatureFromKToC;
+    temperatureFromKToF = (9/5)*(temperature - 273) + 32;
+    temps.push(temperatureFromKToF);
+    console.log (`${temps[0]}K = ${temps[2]}°F = ${temps[1]}°C `);
+}
+
+else {
+    console.log("Error!!!");
+}
+
+
+if (celsiusTemp < 0) {
+    console.log(`ooh it's cold out ${name}, you should wear a ${clothes[5]} or a ${clothes[6]}.`);
+} else if ( celsiusTemp > 40 && celsiusTemp <= 100 ) {
+    console.log(`ooh it's hot out ${name}, I advise you to wear ${clothes[2]}.`);
+} else if ( celsiusTemp > 100) {
+    console.log(`your literally boiling ${saufi}, you should wear ${clothes[0]} or just ${clothes[8]}.`);
+}
 
 
 
