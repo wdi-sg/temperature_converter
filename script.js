e7c50869fd4fab3f7c1144f56b55d854b7c6f2d2
xnithunx <nithun.nanda@gmail.com>
2018-09-05 01:10:48 +0800

    part 2

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var temperature = prompt("Enter the temperature to convert?");
 var temperatureUnit = prompt
 ("What unit of temperature are you measuring? Choose either Fahrenheit, Celsius, or Kelvin")
@@ -6,28 +5,20 @@ var temperatureUnit = prompt
 
 if (temperatureUnit === "celsius") {
 
-   // console.log("The Temperature in Fahrenheit is " + (9/5 * (temperature - 32) + 32))
-   // console.log("The Temperature in Kelvin is " + (temperature + 273))
-
-    var temps = [temperature, ((9/5 * (temperature - 32) + 32), (temperature + 273)]
-    console.log(temps)
+    console.log("The Temperature in Fahrenheit is " + (9/5 * (temperature - 32) + 32))
+    console.log("The Temperature in Kelvin is " + (temperature + 273))
 
 } else if (temperatureUnit === "kelvin") {
-    //console.log("The Temperature in Fahrenheit is " + (9/5 * (temperature - 273) + 32))
-    //console.log("The Temperature in Celsius is " + (temperature - 273))
+    console.log("The Temperature in Fahrenheit is" + (9/5 * (temperature - 273) + 32))
+    console.log("The Temperature in Celsius is " + (temperature - 273))
 
-    var temps = [temperature, (9/5 * (temperature - 273) + 32), (temperature - 273)]
-    console.log(temps)
-
-} else if (temperatureUnit === "fahrenheit") {
-
-    //console.log("The Temperature in Kelvin is " + (5/9 * (temperature - 32) + 273))
-    //console.log("The Temperature in Celsius is " + (5/9 * (temperature - 32)))
-
-    var temps = [temperature, (5/9 * (temperature - 32) + 273), (5/9 * (temperature - 32))]
-    console.log(temps)
+ } else if (temperatureUnit === "fahrenheit") {
+    console.log("The Temperature in Kelvin is " + (5/9 * (temperature - 32) + 273))
+    console.log("The Temperature in Celsius is " + (5/9 * (temperature - 32)))
 
 } else {
 
     console.log ("Please check your temperature entry")
 }
+
+
