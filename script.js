3ded86adf58ecad57f955869310f66963ec3fb38
mervyns <mervyns@gmail.com>
2018-09-04 17:16:46 +0800

    part 2

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-var temperatureInFahr = prompt("What is the temperature in fahrenheits?")
+var temperature = prompt("What is the temperature?")
+var temperatureUnit = prompt("What unit is the temperature in?")
 
-console.log("The Temperature in Kelvin is " + (5/9 * (temperatureInFahr - 32) + 273))
-console.log("The Temperature in Celcius is " + (5/9 * (temperatureInFahr - 32)))
+if (temperatureUnit === "fahrenheit") {
+	console.log("The Temperature in Kelvin is " + (5/9 * (temperature - 32) + 273))
+	console.log("The Temperature in Celcius is " + (5/9 * (temperature - 32)))
+}
+else if (temperatureUnit === "celcius") {
+	console.log("The Temperature in Fahrenheit is " + (9/5 * (temperature - 32) + 32))
+	console.log("The Temperature in Kelvin is " + (temperature + 273))
+} else if (temperatureUnit === "kelvin") {
+	console.log("The Temperature in Fahrenheit is " + (9/5 * (temperature - 273) + 32))
+	console.log("The Temperature in Celcius is " + (temperature - 273))
+} else {
+	console.log("check your temperature unit input")
+}
