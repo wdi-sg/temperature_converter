1aaf5e7bca9d10f896ad6de3dedf4216b14ff172
lamesensei <16118606+lamesensei@users.noreply.github.com>
2018-09-04 17:52:05 +0800

    Add additional prompts, conversion conditionals and decimal function for part 2.

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
+//Part 1
 var temperatureInFahr = prompt('Enter your temperature (f):');
-
 var temperatureInCel = 5/9 * (temperatureInFahr - 32);
 var temperatureInKel = temperatureInCel + 273;
 
-console.log('Celsius: ' + temperatureInCel);
-console.log('Kelvin: ' + temperatureInKel);
+//extra function to convert all numbers to two decimal places.
+function twoDecimalPlaces(number) {
+    return Number.parseFloat(number).toFixed(2);
+}
+
+console.log('Celsius: ' + twoDecimalPlaces(temperatureInCel));
+console.log('Kelvin: ' + twoDecimalPlaces(temperatureInKel));
+
+//Part 2
+var temperature = parseInt(prompt('Enter your starting temperature:'));
+var temperatureUnit = prompt('What unit are you using? (farenheit/celsius/kelvin)');
+
+console.log('Starting temperature is ' + twoDecimalPlaces(temperature) + ' ' + temperatureUnit) + '.';
+
+var c = 0;
+var f = 0;
+var k = 0;
+
+//conditionals to do convertion based on temperature unit
+if (temperatureUnit === 'farenheit') {
+    c = 5/9 * (temperature - 32);
+    f = temperature;
+    k = c + 273;
+} else if (temperatureUnit === 'celsius') {
+    c = temperature;
+    f = 9/5 * temperature + 32;
+    k = c + 273;
+} else if (temperatureUnit === 'kelvin') {
+    c = temperature - 273;
+    f = 9/5 * c + 32;
+    k = temperature;
+} else {
+    console.log('ERROR! Please enter farenheit, celsius or kelvin for the temperature unit.');;
+}
+
+console.log(twoDecimalPlaces(c) +'°C = ' + twoDecimalPlaces(f) + '°F = '+ twoDecimalPlaces(k) + 'K');
