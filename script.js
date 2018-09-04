52618a117e6646cf0cb40585b6db9ee52625bc51
weiwenl <weiwenl.justin@gmail.com>
2018-09-04 19:16:40 +0800

    part 2

--- script.js
+++ script.js
@@ -6,9 +6,40 @@ temperatureInFahr = (9*(temperatureInKelv - 273))/5 + 32; //kel to fahr
 temperatureInKelv = temperature + 273; //deg to kel
 temperature = temperatureInKelv - 273; //kel to deg */
 
-//Part 1
-temperatureInFahr = prompt("Please enter a temperature in Fahrenheit:");
-temperature = temperatureInFahr - 32;
-temperatureInKelv = (5*(temperatureInFahr - 32))/9  + 273;
-console.log(temperature); //represents degrees
-console.log(temperatureInKelv);
+//Part 2
+temperatureRead = prompt("Please enter a temperature:");
+temperatureUnit = prompt("Please enter a temperature unit","Fahrenheit, Celsius, or Kelvin");
+
+if (temperatureUnit === "Fahrenheit"){
+  temperatureInFahr = parseInt(temperatureRead)
+  // F to C
+  temperatureInDeg = parseInt(temperatureRead) - 32;
+  // F to K
+  temperatureInKelv = (5*(parseInt(temperatureRead) - 32))/9  + 273;
+  console.log(temperatureInDeg + "°C ");
+  console.log(temperatureInKelv + "K");
+}
+
+else if (temperatureUnit === "Celsius"){
+  temperatureInDeg = parseInt(temperatureRead);
+  // C to F
+  temperatureInFahr = parseInt(temperatureRead) + 32;
+  // C to K
+  temperatureInKelv = parseInt(temperatureRead) + 273;
+  console.log(temperatureInFahr + "° F ");
+  console.log(temperatureInKelv + "K");
+}
+
+else if (temperatureUnit === "Kelvin"){
+  temperatureInKelv = parseInt(temperatureRead);
+  // K to F
+  temperatureInFahr = (9*(parseInt(temperatureRead) - 273))/5 + 32;
+  // K to C
+  temperatureInDeg = parseInt(temperatureRead) - 273;
+  console.log(temperatureInFahr + "° F ");
+  console.log(temperatureInDeg + "°C ");
+}
+
+else{
+  alert("Incorrect Unit");
+}
