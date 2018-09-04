d8b8c059a35cedbddf346b893a9453e90fd39778
audthenerd <aud.kow88@gmail.com>
2018-09-04 17:58:27 +0800

    added conditional for celsius

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var temperatureInFahr = prompt ("Please enter temperature in Farenheit.");
-var temperatureInKelvin = temperatureInFahr*255.928;
+//var temperatureInFahr = prompt ("Please enter temperature in Farenheit.");
+//var temperatureInKelvin = temperatureInFahr-459.67;
+//var temperatureInCelsius = temperatureInFahr+32;
+//var temperatureInFahr = temperatureInCelsius-17.7778;
 
 var temperature = prompt ("Please enter temperature.");
 var temperatureUnit = prompt ("Please enter temperature unit (Kelvin, Celsius, Farenheit).");
+
+if (temperatureUnit = "Celsius"){
+    var temperatureInKelvin = temperature+273.15;
+    var temperatureInFahr = temperature+32;
+    alert(temperature + ' °C = ' temperatureInKelvin + ' K = ' temperatureInFahr + ' °F');
+}
 
