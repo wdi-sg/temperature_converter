0704d711dd116da94e5c56b0303e22df81b5d8b8
weiwenl <weiwenl.justin@gmail.com>
2018-09-04 20:07:52 +0800

    Part4

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ temperatureInFahr = (9*(temperatureInKelv - 273))/5 + 32; //kel to fahr
 temperatureInKelv = temperature + 273; //deg to kel
 temperature = temperatureInKelv - 273; //kel to deg */
 
-//Part 3
+//Part 4
+var temperatureInFahr;
+var temperatureInDeg;
+var temperatureInKelv;
+var temperatureRead;
+
 var temps = []; //F C K
 temperatureRead = prompt("Please enter a temperature:");
 temperatureUnit = prompt("Please enter a temperature unit","Fahrenheit, Celsius, or Kelvin");
@@ -20,6 +25,7 @@ if (temperatureUnit === "Fahrenheit"){
   temps = [parseInt(temperatureRead), temperatureInDeg, temperatureInKelv];
   console.log(temps[1] + "°C ");
   console.log(temps[2] + "K");
+  printMessage(temps[1])
 }
 
 else if (temperatureUnit === "Celsius"){
@@ -31,6 +37,7 @@ else if (temperatureUnit === "Celsius"){
   temps = [temperatureInFahr, parseInt(temperatureRead), temperatureInKelv];
   console.log(temps[0] + "° F ");
   console.log(temps[2] + "K");
+  printMessage(temps[1])
 }
 
 else if (temperatureUnit === "Kelvin"){
@@ -42,8 +49,21 @@ else if (temperatureUnit === "Kelvin"){
   temps = [temperatureInFahr, temperatureInDeg, parseInt(temperatureRead)];
   console.log(temps[0] + "° F ");
   console.log(temps[1] + "°C ");
+  printMessage(temps[1])
 }
 
 else{
   alert("Incorrect Unit");
+}
+
+function printMessage() {
+  if (temperatureInDeg < 0){
+    console.log("ooh it's cold out");
+  }
+  else if (temperatureInDeg > 40 && temperatureInDeg <= 100){
+    console.log("ooh it's hot out");
+  }
+  else if (temperatureInDeg > 100){
+    console.log("your literally boiling");
+  }
 }
