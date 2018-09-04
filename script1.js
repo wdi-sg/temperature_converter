f5c639f45b61382f335f4442e84cf4149a70c27e
wh_wang@Live.com <wh_wang@live.com>
2018-09-04 17:44:40 +0800

    part 2

---
+++ script1.js
@@ -0,0 +1,26 @@
+var temperature;
+var temperatureUnit;
+var temperatureCelsius;
+var temperatureKelvin;
+var temperatureInFahr;
+
+temperature = prompt("Please enter a temperature");
+temperatureUnit = prompt("please enter the temperature unit");
+
+if (temperatureUnit == "celsius"){
+	temperatureKelvin = temperature+273;
+	temperatureInFahr = (9/5)*temperature+32;
+	console.log(temperature+"C = "+temperatureKelvin+"K = "+temperatureInFahr+"F");
+}
+
+if (temperatureUnit == "kelvin"){
+	temperatureCelsius = temperature-273;
+	temperatureInFahr = (9/5)*(temperature-273)+32;
+	console.log(temperature+"K = "+temperatureCelsius+"C = "+temperatureInFahr+"F");
+}
+
+if (temperatureUnit == "fahrenheit"){
+	temperatureCelsius = (5/9)*(temperature-32);
+	temperatureKelvin = temperatureCelsius+273;
+	console.log(temperature+"F = "+temperatureCelsius+"C = "+temperatureKelvin+"K");
+}
