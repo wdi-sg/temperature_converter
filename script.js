0111b4dff2c0979fdc628d535e6d9541271f3c21
audthenerd <aud.kow88@gmail.com>
2018-09-04 18:29:38 +0800

    Added Kelvin and Fareinheit for Part 2

--- script.js
+++ script.js
@@ -3,12 +3,28 @@
 //var temperatureInCelsius = temperatureInFahr+32;
 //var temperatureInFahr = temperatureInCelsius-17.7778;
 
-var temperature = prompt ("Please enter temperature.");
-var temperatureUnit = prompt ("Please enter temperature unit (Kelvin, Celsius, Farenheit).");
+var temperatureInFahr = prompt("Please enter temperature.");
+
+var temperature = prompt("Please enter temperature.");
+var temperatureUnit = prompt("Please enter temperature unit (Kelvin, Celsius, Farenheit).");
 
 if (temperatureUnit = "Celsius"){
     var temperatureInKelvin = temperature+273.15;
     var temperatureInFahr = temperature+32;
-    alert(temperature + ' °C = ' temperatureInKelvin + ' K = ' temperatureInFahr + ' °F');
+alert(temperature + " degC = " + temperatureInKelvin + " K = " + temperatureInFahr + " degF");
+    } else if (temperature = "Kelvin"){
+        var temperatureInCelsius = temperature-273.15;
+        var temperatureInFahr = temperature-459.67;
+        alert(temperature+" K = " + temperatureInCelsius + " degC = " + temperatureInFahr + " degF");
+    } else if (temperature ="Farenheit"){
+        var temperatureInKelvin = temperature+255.372;
+        var temperatureInCelsius = temperature-17.7778;
+        alert(temperature + " degF = " + temperatureInKelvin + " K = " + temperatureInCelsius + " degC");
+    } else {alert("Invalid input");
 }
+
+
+
+
+
 
