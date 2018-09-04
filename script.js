cff52c4f52352497adcc1bb4e81b3275a72e5bfe
mervyns <mervyns@gmail.com>
2018-09-04 17:50:47 +0800

    part 6

--- script.js
+++ script.js
@@ -36,8 +36,11 @@ if (temperatureUnit === "fahrenheit") {
     // console.log("The Temperature in Kelvin is " + (5/9 * (temperature - 32) + 273))
     // console.log("The Temperature in Celcius is " + (5/9 * (temperature - 32)))
     var temps = [temperature, (5 / 9 * (temperature - 32) + 273), (5 / 9 * (temperature - 32))]
-    console.log(temps)
-    console.log("The Temperature in Kelvin is " + temps[1] + " and the Temperature in Celcius is " + temps[2])
+    var tempunits = ["Celcius", "Fahrenheit", "Kelvin"]
+    for (var i = 0; i < temps.length; i++) {
+        console.log("The temperature is " + temps[i] + " " + tempunits[i]);
+    }
+    // console.log("The Temperature in Kelvin is " + temps[1] + " and the Temperature in Celcius is " + temps[2])
     if (temps[2] < 0) {
         console.log("ooh it's cold out")
         console.log("Hey " + name + ", you should wear " + coldclothes.random())
@@ -56,8 +59,11 @@ if (temperatureUnit === "fahrenheit") {
     // console.log("The Temperature in Fahrenheit is " + (9/5 * (temperature - 32) + 32))
     // console.log("The Temperature in Kelvin is " + (temperature + 273))
     var temps = [temperature, (9 / 5 * (temperature - 32) + 32), (temperature + 273)]
-    console.log(temps)
-    console.log("The Temperature in Kelvin is " + temps[2] + " and the Temperature in Fahrenheit is " + temps[1])
+    var tempunits = ["Celcius", "Fahrenheit", "Kelvin"]
+    for (var i = 0; i < temps.length; i++) {
+        console.log("The temperature is " + temps[i] + " " + tempunits[i]);
+    }
+    // console.log("The Temperature in Kelvin is " + temps[2] + " and the Temperature in Fahrenheit is " + temps[1])
     if (temps[0] < 0) {
         console.log("ooh it's cold out")
         console.log("Hey " + name + ", you should wear " + coldclothes.random())
@@ -75,8 +81,11 @@ if (temperatureUnit === "fahrenheit") {
     // console.log("The Temperature in Fahrenheit is " + (9/5 * (temperature - 273) + 32))
     // console.log("The Temperature in Celcius is " + (temperature - 273))
     var temps = [temperature, (9 / 5 * (temperature - 273) + 32), (temperature - 273)]
-    console.log(temps)
-    console.log("The Temperature in Fahrenheit is " + temps[1] + " and the Temperature in Celcius is " + temps[2])
+    var tempunits = ["Celcius", "Fahrenheit", "Kelvin"]
+    for (var i = 0; i < temps.length; i++) {
+        console.log("The temperature is " + temps[i] + " " + tempunits[i]);
+    }
+    // console.log("The Temperature in Fahrenheit is " + temps[1] + " and the Temperature in Celcius is " + temps[2])
     if (temps[2] < 0) {
         console.log("ooh it's cold out")
         console.log("Hey " + name + ", you should wear " + coldclothes.random())
