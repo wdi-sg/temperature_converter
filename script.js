230748d2995a58b0daf78450f4aef983e0027bbb
xnithunx <nithun.nanda@gmail.com>
2018-09-04 19:37:46 +0800

    creating the converters

---
+++ script.js
@@ -0,0 +1,18 @@
+var temperature = prompt("Enter the temperature to convert?");
+var temperatureUnit = prompt
+("What unit of temperature are you measuring? Choose either Fahrenheit, Celsius, or Kelvin")
+
+
+if (temperatureUnit === "celsius") {
+
+    console.log("The Temperature in Fahrenheit is " + (9/5 * (temperature - 32) + 32))
+    console.log("The Temperature in Kelvin is " + (temperature + 273))
+
+} else if (temperatureUnit === "kelvin") {
+    console.log("The Temperature in Fahrenheit is" + (9/5 * (temperature - 273) + 32))
+    console.log("The Temperature in Celsius is " + (temperature - 273))
+
+ } else if (temperatureUnit === "fahrenheit") {
+    console.log("The Temperature in Kelvin is " + (5/9 * (temperature - 32) + 273))
+    console.log("The Temperature in Celcius is " + (5/9 * (temperature - 32)))
+}
