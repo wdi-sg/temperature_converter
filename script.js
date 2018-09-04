b90d574e1df495d9a0ae9ff837fecc45130c0646
audthenerd <aud.kow88@gmail.com>
2018-09-04 17:33:00 +0800

    added Part I and Part II

---
+++ script.js
@@ -0,0 +1,6 @@
+var temperatureInFahr = prompt ("Please enter temperature in Farenheit.");
+var temperatureInKelvin = temperatureInFahr*255.928;
+
+var temperature = prompt ("Please enter temperature.");
+var temperatureUnit = prompt ("Please enter temperature unit (Kelvin, Celsius, Farenheit).");
+
