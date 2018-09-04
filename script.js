f8b82215e85e9a22368ce56cda56b53599b9ec13
ah5ive <ah5ive@gmail.com>
2018-09-04 20:25:47 +0800

    part 2

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-var temperatureInFahr = prompt ("What is your temperature now?");
+/*var temperatureInFahr = prompt ("What is your temperature now?");
 var Celsius = 5/9 * (temperatureInFahr - 32);
 var Kelvin = 5/9 * (temperatureInFahr - 32) + 273;
 
 console.log(Celsius + " " + "Celsius");
 console.log(Kelvin + " " + "Kelvin");
+*/
+var temperature = prompt("What your temperature in degrees");
+var temperatureUnit = prompt("Fahrenheit, Celsius or Kelvin");
+
+
+if (temperatureUnit="Celsius") {
+console.log(9/5 * (temperature + 32) + " "+ "Fahrenheit");
+console.log((temperature + 273) + " " + "Kelvin");
+}
+
+else if (temperatureUnit = "Kelvin"){
+    console.log(9/5 *(temperature - 273) + 32+ " " + "Fahrenheit");
+    console.log(temperature - 273+" "+"Celsius" );
+}
+
+else if (temperatureUnit = "Fahrenheit"){
+    console.log((5/9 (temperature - 32) + 273)+ " " +"Kelvin");
+    console.log(5/9 (temperature - 32) + " " + "Celsius");
+}
