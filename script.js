074ebbe54261aa96e77a6c2b8f2fee23955334a9
Rstinkho <t.turchinskiy@bk.ru>
2018-09-04 23:14:28 +0800

    Task 1,2,4,5 of assignment

---
+++ script.js
@@ -0,0 +1,60 @@
+/* Task 1 (delete comment tags to run the code)
+
+var temperatureInFahr = prompt("Enter the temperature in Fahrenheit");
+
+var temperatureInCelsius = ((temperatureInFahr - 32) / 1.8);
+console.log(temperatureInCelsius);
+
+var temperatureInKelvin = (temperatureInCelsius + 273.15);
+console.log(temperatureInKelvin);
+*/
+
+// Task 2 Define formula for all possible user input (var clothes define for Task 5)
+
+var temperature = prompt ("Please enter the temperature value");
+var temperatureUnit = prompt ("Please enter fahrenheit, celsius or kelvin");
+var clothes = [
+ 'nothing',
+ 'swimsuit',
+ 'shorts and shirt',
+ 'sweater',
+ 'sweater + jacket',
+ 'heavy jacket',
+ 'heavy jacket and toe warmers',
+ 'dont go outside if you want to live'
+];
+
+if (temperatureUnit === "fahrenheit") {
+    console.log("Temperature in Fahrenheit is " + temperature);
+    console.log("Temperature in Celsius is " + (temperature - 32) / 1.8);
+    console.log("Temperature in Kelvin is " + temperature * 8.53);
+} else if (temperatureUnit === "celsius") {
+    console.log("Temperature in Celsius is " + temperature);
+    console.log("Temperature in Fahrenheit is " + (temperature * 1.8 + 32));
+    console.log("Temperature in Kelvin is " + 273.15);
+} else if (temperatureUnit === "kelvin") {
+    console.log("Temperature in Kelvin is " + temperature);
+    console.log("Temperature in Celsius is " + (temperature - 273.15));
+    console.log("Temperature in Fahrenheit " + ((temperature - 273.15) * 1.8 + 32));
+} else {
+    console.log("Please enter proper temperature type")
+};
+
+// Task 4 & 5
+var name = prompt("What is your name?")
+if (temperature <= 0) {
+    console.log("ooh its cold out");
+    console.log(name + " "  + "you should wear " + clothes[6]);
+} else if (temperature > 40 && temperature < 100) {
+    console.log("ooh it's hot out");
+    console.log(name + " "  + "you should wear " + clothes[2]);
+} else if (temperature > 100) {
+    console.log ("You are literally burning!");
+    console.log(name + " "  + "you should wear " + clothes[0]);
+} else {
+    console.log("CRITICAL ERROR");
+};
+
+
+
+
