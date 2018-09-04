6d2e1e110484a91bcdc2761e94f1e0d57f423c77
mervyns <mervyns@gmail.com>
2018-09-04 17:09:22 +0800

    part 1

---
+++ script.js
@@ -0,0 +1,4 @@
+var temperatureInFahr = prompt("What is the temperature in fahrenheits?")
+
+console.log("The Temperature in Kelvin is " + (5/9 * (temperatureInFahr - 32) + 273))
+console.log("The Temperature in Celcius is " + (5/9 * (temperatureInFahr - 32)))
