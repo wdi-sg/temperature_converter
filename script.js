c5da91160e186367ecc88af823c38a03e403d067
manieshablakey <manieshab@gmail.com>
2018-09-16 15:06:44 +0800

    modified part 1-3

--- script.js
+++ script.js
@@ -1,36 +1,55 @@
+//Part 1: ask user for temperature in Farenheit. Log input into Kelvin and Celsius.
+var temperatureinFahr = prompt("What is the temperature in Farenheit? Enter a number.");
+console.log("The temperature is " + ((temperatureinFahr - 32) * (5/9)) + " degrees Celsius.");
 
-var temperatureinFahr = prompt("What is the temperature in Farenheit?")
-	console.log("The temperature is " + ((temperatureinFahr - 32) * (5/9)) + " degrees Celsius.");
-    console.log("The temperature is " + ((temperatureinFahr + 459.57) * (5/9)) + " Kelvin.");
+ //Part 2: Prompt user for starting temperature in numerical value. Prompt user for starting temperature unit in Farenheit, Celsius or Kelvin.
+var temperature = prompt("What is the temperature? Enter a numerical value");
+var temperatureUnit = prompt("What unit is the temperature measured in? Enter Farenheit, Celsius or Kelvin");
 
-var temperature = prompt("What is the temperature?");
-var temperatureUnit = prompt("What unit is the temperature measured in?");
-
-if (temperatureUnit === "farenheit") {
-	console.log("The temperature is " + ((temperature - 32) * (5/9)) + " degrees Celsius.");
-    console.log("The temperature is " + ((temperature + 459.57) * (5/9)) + " Kelvin.");
-} else if (temperatureUnit === "celsius") {
-	console.log("The temperature is " + ((1.8 * temperature) + 32) + " degrees Farenheit.");
-	console.log("The temperature is " + (temperature + 273.15) + " Kelvin.");
-} else if (temperatureUnit === "kelvin") {
-	console.log("The temperature is " + (temperature - 273.15) + " degrees Celsius.");
-	console.log("The temperature is" + ((temperature * 9/5) - 459.67) + " degrees Farenheit.");
+function farenheit() {
+	var farenheitToCelsius = ((temperature - 32) * (5/9));
+	var farenheitToKelvin = ((temperature + 459.57) * (5/9));
+	if (temperatureUnit == "farenheit") {
+		console.log(`The temperature is ${farenheitToCelsius} degrees Celsius.`);
+		console.log(`The temperature is ${farenheitToKelvin} degrees Kelvin.`);
+	} 
 }
+farenheit();
 
-var temps = [[temperature, ((temperature - 32) * (5/9)), ((temperature + 459.57) * (5/9))], [temperature, ((1.8 * temperature) + 32), (temperature + 273.15)], [temperature, (temperature - 273.15), ((temperature * 9/5) - 459.67)]];
-var userName = prompt("What is your name?");
-var clothes = ["nothing", "swimsuit", "shorts and shirt", "sweater", "sweater + jacket", "heavy jacket", "heavy jacket and toe warmers", "don't got outside if you want to live"];
-
-if (temps[[0]] > 100) {
-	console.log("You're literally boiling.");
-	console.log("Consider wearing " + clothes[0] + " or " + clothes[1] + ".");
-} else if (temps[[0]] > 40) {
-	console.log("Ooh, it's hot out.");
-	console.log("Consider wearing " + clothes[1] + ".");
-} else if (temps[[0]] < 0) {
-	console.log("Ooh, it's cold out.");
-	console.log("Consider wearing " + clothes[4] + " or " + clothes[5] + ".");
+function celsius() {
+	var celsiusToFarenheit = ((temperature * 9/5) + 32);
+	var celsiusToKelvin = (temperature + 273.13);
+	if (temperatureUnit == "celsius") {
+		console.log(`The temperature is ${celsiusToFarenheit} degrees Farenheit.`);
+		console.log(`The temperature is ${celsiusToKelvin} degrees Kelvin.`);
+	}
 }
+celsius();
+
+function kelvin() {
+	var kelvinToCelsius = (temperature - 273.15);
+	var kelvinToFarenheit = (((temperature - 273.15) * (9/5)) + 32);
+	if (temperatureUnit == "kelvin") {
+		console.log(`The temperature is ${kelvinToCelsius} degrees Celsius.`);
+		console.log(`The temperature is ${kelvinToFarenheit} degrees Farenheit.`);
+	}
+}
+kelvin();
+
+//Part 3: 
+var startingTemp = prompt("Enter a temperature.");
+var temperatureUnit = prompt("What unit is the temperature measured in? Enter Farenheit, Celsius or Kelvin");
+var temps = [startingTemp, ((startingTemp * 9/5) + 32), (startingTemp + 273.13)];
+
+function celsius() {
+	if ((startingTemp == temps[0]) && temperatureUnit == "celsius") {
+		console.log(`The temperature is ${temps[1]} degrees Farenheit.`);
+		console.log(`The temperature is ${temps[2]} degrees Kelvin.`);
+	}
+}
+celsius();
+
+
 
 
 
