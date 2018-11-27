81b8659db7e2b3fa35427d99573036bf0552b444
Ishak <ishak@Ishaks-MacBook-Pro.local>
2018-11-27 21:00:30 +0800

    created index.html and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var temperature = prompt("What's is the temperature now?(in farenheit)");
+    console.log(temperature);
+
+var tempInCelsius = ;
+var tempInKelvin = ;
+
+alert("something is said!")
