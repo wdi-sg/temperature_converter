33ce18688019d5b9dba4bbb712137e04b916bbef
kingcashthefifth <kingcashthefifth@gmail.com>
2018-11-27 17:28:45 +0800

    part 1 done

---
+++ script.js
@@ -0,0 +1,5 @@
+var temperatureInFahr = prompt("What is the Fahrenheit temperature you want converted?");
+
+var convertToDeg = temperatureInFahr * 9/5 + 32;
+
+alert(temperatureInFahr + " Fahrenheit is " + convertToDeg + " in Degrees.");
