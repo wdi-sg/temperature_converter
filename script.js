8d8e78b1e3fac06e093ff3f271b21eb5742e7700
Ishak <ishak@Ishaks-MacBook-Pro.local>
2018-11-27 22:34:51 +0800

    part 1, ask for temperature input and show conversion in Celcius & Kelvin

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var temperature = prompt("What's is the temperature now?(in farenheit)");
-    console.log(temperature);
+var temperature = prompt("Please input the temperature (farenheit):");
+var tempInFahr = parseInt(temperature); //change string to number and store in var tempInFahr
+    console.log(tempInFahr);
 
-var tempInCelsius = ;
-var tempInKelvin = ;
+var tempInCelsius = ((tempInFahr - 32) / 1.8); //convert to Celcius
+var tempInKelvin = (((tempInFahr - 32) / 1.8) + 273.15); //convert to Kelvin
 
-alert("something is said!")
+alert("Temperature in degree Celcius is " + tempInCelsius.toFixed(2) + "\nTemperature in Kelvin is " + tempInKelvin.toFixed(2));
