7b56e1d039e6c19399218e1f378654a3d8832e77
scottosan <scottieseah@gmail.com>
2018-11-28 08:24:27 +0800

    working alert and basic calculation

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-var userInput = prompt('What's your name?');
-console.log( userInput );
+var temperature = prompt('Temperature?');
+console.log( "calculating" );
+var unit = prompt('Unit?');
+var celsius = temperature * 0.5;
+var kelvin = temperature * 0.25;
+var farhenheit = temperature * 0.7;
+switch(unit){
+    case "K":
+        alert(temperature + " k = " + celsius + " c = " + farhenheit + " f");
+        break;
+    case "c":
+        alert(kelvin + " K " + farhenheit + "° f ");
+        break;
+    case "f":
+        alert(celsius + "° c " + kelvin + " k");
+        break;
+    default:
+        alert("please enter unit");
+    };
