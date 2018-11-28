3267f2339859a4c3d8871ecb155e41af4f1a55f2
hongchiong <hongchiong@gmail.com>
2018-11-28 08:59:57 +0800

    completed Part 3

--- script.js
+++ script.js
@@ -6,27 +6,30 @@
 
 // alert("Temperature in Kelvins is:" + temperatureInK + "\n" + "Temperature in Celcius is: " + temperatureInC);
 
-var temperature = prompt("Please enter temperature degrees");
+var STARTING_TEMP = prompt("Please enter temperature degrees");
 var temperatureUnit = prompt("Please enter temperature units");
 
 switch (temperatureUnit.toLowerCase()) {
     case 'fahrenheits':
-        var temperatureInK = ((Number(temperature) - 32) * (5/9)) + 273.15;
-        var temperatureInC = ((Number(temperature) - 32) * (5/9));
-        alert("Temperature in Kelvins is:" + temperatureInK + "\n" + "Temperature in Celcius is: " + temperatureInC);
+        var CONVERTED_TEMP_1 = ((Number(STARTING_TEMP) - 32) * (5/9)) + 273.15;
+        var CONVERTED_TEMP_2 = ((Number(STARTING_TEMP) - 32) * (5/9));
+        alert("Temperature in Kelvins is:" + CONVERTED_TEMP_1 + "\n" + "Temperature in Celcius is: " + CONVERTED_TEMP_2);
         break;
 
     case 'kelvins':
-        var temperatureInF = ((Number(temperature) - 273.15)*(9/5)+32);
-        var temperatureInC = Number(temperature) - 273.14;
-        alert("Temperature in Fahrenheits is: " + temperatureInF + "\n" + "Temperature in Celcius is: " + temperatureInC);
+        var CONVERTED_TEMP_1 = ((Number(STARTING_TEMP) - 273.15)*(9/5)+32);
+        var CONVERTED_TEMP_2 = Number(STARTING_TEMP) - 273.14;
+        alert("Temperature in Fahrenheits is: " + CONVERTED_TEMP_1 + "\n" + "Temperature in Celcius is: " + CONVERTED_TEMP_2);
         break;
 
     case 'celcius':
-        var temperatureInK = Number(temperature) + 273.15;
-        var temperatureInF = (Number(temperature)*(9/5))+32;
-        alert("Temperature in Fahrenheits is: " + temperatureInF + "\n" + "Temperature in Kelvins is: " + temperatureInK);
+        var CONVERTED_TEMP_1 = Number(STARTING_TEMP) + 273.15;
+        var CONVERTED_TEMP_2 = (Number(STARTING_TEMP)*(9/5))+32;
+        alert("Temperature in Kelvins is: " + CONVERTED_TEMP_1 + "\n" + "Temperature in Fahrenheits is: " + CONVERTED_TEMP_2);
 
     default:
         alert("Please check your inputs. Temperature should be numbers. Temperature units should be Kelvins, Fahrenheits or Celcius");
 }
+
+var temps = [STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2];
+console.log(temps[1]);
