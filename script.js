570d9f384a482ca99ebc54bb1af02fd602baa8db
adrian7296 <adriantjs7296@gmail.com>
2018-12-02 00:08:39 +0800

    completed the basic

--- script.js
+++ script.js
@@ -1,40 +1,81 @@
-//Part 1
+// Part 1
+// try to prompt user to answer the name , however once user click "cancel" , I don know how to stop the rest of game for running
 
-var  userName = prompt("Hi what your name?", "User");
+var  userName = prompt("Hi what your name?");
     if (userName == null) {
-    userName = 'User';
-}
+        alert("Thank for playing ! ")
+    }
+    else if (userName == "") {
+    userName = "User";
+    }
 
-var temperatureInFahr =  prompt("Hi " + userName + " , may I know the current temperature now in Fahrenheit? ");
-var kelvin = (temperatureInFahr - 32 ) * 5/9 + 273.15;
-var celsius = (temperatureInFahr -32) * 5/9;
-    console.log( "The current temperature is " + temperatureInFahr + "F");
+
+var temperatureInFahr =  prompt("Hi " + userName + " , what the temperature now in Fahrenheit? ");
+var Fahr = parseFloat(temperatureInFahr).toFixed(2);
+var kelvin = parseFloat(((Fahr - 32 ) * 5/9 + 273.15)).toFixed(2);
+var celsius = parseFloat(((Fahr -32) * 5/9)).toFixed(2);
     console.log("The current temperature is " + kelvin+ "K");
+    console.log( "The current temperature is " + celsius + "C");
+    console.log("The current temperature is " + Fahr + "F")
+    alert("The temperature is " + kelvin+ " K= " + celsius + "C= " + Fahr + "F");
 
-//Part 2
+//Part 2& Part 3
+var storeTemp = [ ];
 var temperatureUnit = ["Fahrenheit", "Celsius" , "Kelvin"];
 var tempUnit = prompt("What's your temperature unit? Fahrenheit,Celsius,Kelvin");
-var temperature= prompt ("What's is current temperature?");
-if (tempUnit === temperatureUnit[0])
-{
-    console.log((temperature- 32 ) * 5/9 + 273.15 + "K");
-    console.log((temperature- 32 ) * 5/9 + "C");
+var temperature = prompt("What's is current temperature?");
+if (tempUnit === temperatureUnit[0]) {
+    var Fah = ((temperature) + "F");
+    var Kel = (temperature- 32 ) * 5/9 + 273.15 + "K";
+    var Cel = (temperature- 32 ) * 5/9 + "C";
+    storeTemp.push(Fah);
+    storeTemp.push(Cel);
+    storeTemp.push(Kel);
 }
-else if(tempUnit === temperatureUnit[1])
-{
-    console.log(temperature + 273.15 + "K");
-    console.log(temperature * 9/5 + 32 + "F");
+else if(tempUnit === temperatureUnit[1]) {
+    var Cel = ((temperature) + "C");
+    var Kel = (temperature + 273.15 + "K");
+    var Fah = (temperature * 9/5 + 32 + "F");
+    storeTemp.push(Fah);
+    storeTemp.push(Cel);
+    storeTemp.push(Kel);
 }
-else if(tempUnit === temperatureUnit[2])
-{
-    console.log(temperature - 273.15 + "C"); // Kelvin to Celsius
-    console.log((temperature -273.15) * 9/5 +32 + "F"); //Kelvin to Fahren
+else if(tempUnit === temperatureUnit[2]) {
+    var Kel = ((temperature) + "K");
+    var Cel = (temperature) - 273.15 + "C";
+    var Fah = (temperature) -273.15 * 9/5 +32 + "F";
+    storeTemp.push(Kel);
+    storeTemp.push(Cel);
+    storeTemp.push(Fah);
 }
-else
-{
+else {
     console.log("Invalid");
+    alert("Invalid, please key in the exact words ")
 }
 
-//Part 3
+var clothes = [
+ 'nothing',
+ 'swimsuit',
+ 'shorts and shirt',
+ 'sweater',
+ 'sweater + jacket',
+ 'heavy jacket',
+ 'heavy jacket and toe warmers',
+ 'dont go outside if you want to live'
+];
 
-var temps = [ STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2 ];
+if (storeTemps[2] < 0){
+    alert("ohh it's cold out!");
+    clothes.push(   )
+}
+else if( storeTemps[2] > 40){
+    alert("ohh it's hot out!");
+}
+else if(storeTemps[2] > 100){
+    alert("It is literally boiling!");
+}
+else{
+    alert("Shiok !!");
+
+
+
