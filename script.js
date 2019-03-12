726d3513191c4d1612c607f7797217aae1527c5b
tartufu <ainurlindale@gmail.com>
2019-03-12 21:56:46 +0800

    added part 3

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 var temperatureInFar = prompt("What is the temperature in Farenheit now?"); //gets F temp in numbers
 var temparatureInCel = (temperatureInFar - 32) * 5 / 9; //formula
 var temperatureinKel = ((temperatureInFar - 32) * 5 / 9) + 273.15; //formula
+var temps = []; //empty array to store values
+
 
 console.log(temparatureInCel + "C");
 console.log(temperatureinKel + "K");
@@ -11,21 +13,45 @@ alert(`the temperature in Celsius is ${temparatureInCel}C. The temperature in Ke
 //PART 2
 var temperature = prompt("What is the temperature now?");
 var temperatureUnit = prompt("is the temp given in Celsius, Kelvin or Fahrenheit. Note: Only type C, K or F! ");
+var startTemps = temperature + temperatureUnit;
+temps.push(startTemps);
 
 if (temperatureUnit === "F") {
     temparatureInCel = (temperature - 32) * 5 / 9;
     temperatureinKel = ((temperature - 32) * 5 / 9) + 273.15;
     alert(`the temperature in Celsius is ${temparatureInCel}C. The temperature in Kelvin is ${temperatureinKel}K. `);
+
+    var tempsCel = (temparatureInCel + "C");
+    var tempsKel = (temperatureinKel + "K");
+    temps.push(tempsCel);
+    temps.push(tempsKel);
+
 }; //BLOCK SCOPE FOR F to C and K. Reassign var value from top
 
 if (temperatureUnit === "C") {
-    temperatureinKel = Number(temperature) + 273.15; //to check, why does temparature in this section cats value together without Number()
+    temperatureinKel = Number(temperature) + 273.15; //to check, why does temperature in this section cats value together without Number()
     temperatureInFar =(temperature * 9 / 5) + 32;
     alert(`the temperature in Fahrenheit is ${temperatureInFar}F. The temperature in Kelvin is ${temperatureinKel}K. `);
+
+    var tempsKel = (temperatureinKel + "K");
+    var tempsFar = (temperatureInFar + "F");
+    temps.push(tempsKel);
+    temps.push(tempsFar);
 }; // BLOCKSCOPE for C to F and K. Reassign var value from top
 
 if (temperatureUnit === "K") {
     temparatureInCel = temperature - 273.15;
     temperatureInFar = ((temperature - 273.15) * 9 / 5) + 32;
     alert(`the temperature in Kelvin is ${temperatureInFar}F. The temperature in Celsius is ${temparatureInCel}C. `);
+
+    var tempsCel = (temparatureInCel + "K");
+    var tempsFar = (temperatureInFar + "F");
+    temps.push(tempsCel);
+    temps.push(tempsFar);
 }; // BLOCKSCOPE for K to F and C. Reassign var value from top
+
+//PART 3
+// array for temp is kept at the top
+// push  temp at part 2
+//
+console.log(temps);
