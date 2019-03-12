fd3e84de0f858dd4bbe77905671f05cd54c70501
moses <moseslee90@gmail.com>
2019-03-12 06:39:55 -0700

    fixed if functions having a variable input instead of string input for f c k in condition evaluation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var temperature = prompt("Enter Temperature in Degrees:"
+var temperature = prompt("Enter Temperature in Degrees:");
 console.log(temperature);
 var temperatureUnit = prompt("Enter Unit of Temperature to use, 'f' for Farenheit, 'c' for Celcius and 'k' for Kelvin");
 console.log(temperatureUnit);
@@ -23,25 +23,23 @@ var kelvinToFahr = function (kelvin) {
 var farenheit = 0;
 var kelvin = 0;
 var celcius = 0;
-alert("")
 //if farenheit, run functions for converting farenheit to other units
-if (temperatureUnit === f) {
+if (temperatureUnit === "f") {
     farenheit = temperature;
     celcius = fahrToCelcius(temperature);
     kelvin = fahrToKelvin(temperature);
 //if celcius, run function for converting celcius to other units
-} else if (temperatureUnit === c) {
+} else if (temperatureUnit === "c") {
     farenheit = celciusToFahr(temperature);
     celcius = temperature;
     kelvin = celciusToKelvin(temperature);
 //if kelvin, run functions for converting kelvin to other units
-} else if (temperatureUnit === k) {
+} else if (temperatureUnit === "k") {
     farenheit = kelvinToFahr(temperature);
     celcius = kelvinToCelcius(temperature);
     kelvin = temperature;
 }
-
-
+alert(farenheit + "o F = " + celcius + " oC = " + kelvin + "K");
 //set 3 vars to their respective values after conditional conversion
 
 // alert("You entered " + temperature + "Farenheit, which is " + fahrToCelcius(temperature) + " Celcius.");
