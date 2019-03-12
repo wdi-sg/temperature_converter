9dd4376084be9c90d1c832b4a4cd2e674b952443
seeo <aragonlotr5@gmail.com>
2019-03-12 19:20:34 +0800

    created two prompts in js.

---
+++ script.js
@@ -0,0 +1,13 @@
+var temperatureInFahr = prompt("What's the temperature in Fahrenheit?");
+console.log(temperatureInFahr);
+
+var temperatureInCelsius = (temperatureInFahr-32) * (5/9);
+var temperatureInKelvin = temperatureInCelsius + 273.15;
+
+alert("The temperature in celsius is "+temperatureInCelsius+"c."+" "+"While the temperature in kelvin is "+temperatureInKelvin+"k.");
+
+var temperature = prompt("What is the temperature in (any) degrees?")
+console.log(temperature);
+
+var temperatureInUnit = prompt("Now what unit will this temperature take?")
+console.log(temperature);
