37cddb7542cf04609d908b48aa0b2681bb6ffe7a
tartufu <ainurlindale@gmail.com>
2019-03-12 21:16:51 +0800

    Completed Part 1

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // script.js
 
-var userInput = prompt("What's your name?");
-console.log(userInput);
-alert(`Your name is ${userInput}`);
+var temperatureInFar = prompt("What is the temperature in Farenheit now?"); //gets F temp in numbers
+var temparatureInCel = (temperatureInFar - 32) * 5 / 9; //formula
+var temperatureinKel = ((temperatureInFar - 32) * 5 / 9) + 273.15; //formula
+
+console.log(temparatureInCel + "C");
+console.log(temperatureinKel + "K");
+alert(`the temperature in Celsius is ${temparatureInCel}C. The temperature in Kelvin is ${temperatureinKel}K. `); // how to line break without /n?
