5e5c8d87cef76c261c23ee6f950aca8b39fcab1f
seantanjs <seantanjuisiang@gmail.com>
2019-03-12 18:00:41 +0800

    Completed part 1 of assignment

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Assignment1")
+
+var temperatureInFahr = prompt('What is the temperature in F?');
+
+var temperatureInCel = (temperatureInFahr - 32) * (5/9);
+
+alert("userInput in F: " + temperatureInFahr + "\n" + "userInput in C: " + temperatureInCel)
