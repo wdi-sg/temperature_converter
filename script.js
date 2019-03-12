5065a47fa3ffd5a3b6568426c12def0321d8a975
khyreerusydi <nicoraventbh@gmail.com>
2019-03-12 22:09:04 +0800

    added additional parts

--- script.js
+++ script.js
@@ -15,27 +15,42 @@ console.log( "The temperature to be converted is " + temperature + temperatureUn
 //else prompt for unit again
 
 if (temperatureUnit === "C" || temperatureUnit === "c") {
-    var outputF = (temperature*1.8)+32;
-    var outputK = temperature+273.15;
-    document.getElementById("tempF").innerHTML = outputF;
+    var output1 = (temperature*1.8)+32;
+    var output2 = temperature+273.15;
+    document.getElementById("tempF").innerHTML = output1;
     document.getElementById("tempC").innerHTML = temperature;
-    document.getElementById("tempK").innerHTML = outputK;
+    document.getElementById("tempK").innerHTML = output2;
 }
 else if (temperatureUnit === "F" || temperatureUnit === "f") {
     console.log("hello")
-    var outputC = (temperature-32)/1.8;
-    var outputK = ((temperature-32)/1.8)+273.15;
+    var output1 = (temperature-32)/1.8;
+    var output2 = ((temperature-32)/1.8)+273.15;
     document.getElementById("tempF").innerHTML = temperature;
-    document.getElementById("tempC").innerHTML = outputC;
-    document.getElementById("tempK").innerHTML = outputK;
+    document.getElementById("tempC").innerHTML = output1;
+    document.getElementById("tempK").innerHTML = output2;
 }
 else if (temperatureUnit === "K" || temperatureUnit === "k") {
-    var outputC = temperature-273.15;
-    var outputF = ((temperature-273.15)*1.8)+32;
-    document.getElementById("tempF").innerHTML = outputF;
-    document.getElementById("tempC").innerHTML = outputC;
+    var output1 = temperature-273.15;
+    var output2 = ((temperature-273.15)*1.8)+32;
+    document.getElementById("tempF").innerHTML = output2;
+    document.getElementById("tempC").innerHTML = output1;
     document.getElementById("tempK").innerHTML = temperature;
 }
 else {
     var temperatureUnit = alert("Please key in temperature's unit as 'C', 'F', or 'K'.");
 };
+
+var STARTING_TEMP = parseFloat(temperature);
+var CONVERTED_TEMP_1 = parseFloat(output1);
+var CONVERTED_TEMP_2 = parseFloat(output2);
+
+var temps = [ STARTING_TEMP, CONVERTED_TEMP_1, CONVERTED_TEMP_2 ];
+console.log(temps)
+
+if (temperature < 0) {
+    document.getElementById("howCold").innerHTML = "ooh it's cold out!";
+} else if (temperature > 40 && temperature < 100) {
+    document.getElementById("howCold").innerHTML = "ooh it's hot out!";
+} else  if (temperature >= 100) {
+    document.getElementById("howCold").innerHTML = "your're literally boiling!";
+}
