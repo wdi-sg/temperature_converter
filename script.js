88316b7d2f2f493872ed30f4c53afdabef0c9d31
moses <moseslee90@gmail.com>
2019-03-12 06:13:11 -0700

    added fahrToKelvin, completed Part 1

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var temperatureInFahr = prompt("Enter Temperature in Farenheit:");
-console.log(temperatureInFahr);
+var temperature = prompt("Enter Temperature in Degrees:"
+console.log(temperature);
+var temperatureUnit = prompt("Enter Unit of Temperature to use, 'f' for Farenheit, 'c' for Celcius and 'k' for Kelvin");
 var fahrToCelcius = function (fahr) {
     return (fahr - 32)*5/9;
 }
-alert("You entered " + temperatureInFahr + "Farenheit, which is " + fahrToCelcius(temperatureInFahr) + " Celcius.");
+var fahrToKelvin = function (fahr) {
+    return fahrToCelcius+273.15;
+}
+alert("You entered " + temperature + "Farenheit, which is " + fahrToCelcius(temperature) + " Celcius.");
