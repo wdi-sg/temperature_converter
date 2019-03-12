39644e663565a059afbb817de58dadea0de6ec1b
chuasweechin <sampiochua@gmail.com>
2019-03-12 23:17:07 +0800

    add in part 4,5,6 and 7

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function part1 () {
+function part_1 () {
     var userInputTemperatureInFahr = parseInt(prompt("What is the temperature in Fahrenheit?", "Please enter a number"));
     console.log("User have input " + userInputTemperatureInFahr + "as the temperature in Fahrenheit.");
 
@@ -13,7 +13,7 @@ function part1 () {
     console.log(temperatureInFahrDisplay + " = " + temperatureInCelDisplay + " = " + temperatureInKelDisplay);
 }
 
-function part2 () {
+function part_2 () {
     var temperatureInCelDisplay;
     var temperatureInKelDisplay;
     var temperatureInFahrDisplay;
@@ -51,50 +51,94 @@ function part2 () {
         temperatureInFahrDisplay = temperatureInFahr.toFixed(2) + "\xB0F";
 
     }
+
     console.log(temperatureInFahrDisplay + " = " + temperatureInCelDisplay + " = " + temperatureInKelDisplay);
-
 }
 
-function part3 () {
+function part_3_4_5_6_7 () {
     var temps = [];
+    var cont = true;
+    var temperature = 0;
+    var clothes = [
+         "nothing",
+         "swimsuit",
+         "shorts and shirt",
+         "sweater",
+         "sweater + jacket",
+         "heavy jacket",
+         "heavy jacket and toe warmers",
+         "don't go outside if you want to live"
+    ];
 
-    var userInputTemperature = parseInt(prompt("What is the temperature?", "Please enter a number"));
-    var userInputTemperatureUnit = prompt("What is the unit of measurement?", "Please enter either Fahrenheit, Celsius, or Kelvin");
+    while (cont) {
+        var userInputName = prompt("What is your name?");
+        var userInputTemperature = parseInt(prompt("What is the temperature?", "Please enter a number"));
+        var userInputTemperatureUnit = prompt("What is the unit of measurement?", "Please enter either Fahrenheit, Celsius, or Kelvin");
 
-    console.log("User have input " + userInputTemperature + " as the temperature in " + userInputTemperatureUnit + ".");
+        console.log("User have input " + userInputTemperature + " as the temperature in " + userInputTemperatureUnit + ".");
 
-    if (userInputTemperatureUnit === "Fahrenheit"){
-        var temperatureInKel =(userInputTemperature - 32) * 5/9 + 273.15;
-        var temperatureInCel =(userInputTemperature - 32) * 5/9;
+        if (userInputTemperatureUnit === "Fahrenheit"){
+            var temperatureInKel =(userInputTemperature - 32) * 5/9 + 273.15;
+            var temperatureInCel =(userInputTemperature - 32) * 5/9;
 
-        // process data into display format
-        temps.push(userInputTemperature.toFixed(2) + "\xB0F");
-        temps.push(temperatureInKel.toFixed(2) + "K");
-        temps.push(temperatureInCel.toFixed(2) + "\xB0C");
+            // process data into display format
+            temps.push(userInputTemperature.toFixed(2) + "\xB0F");
+            temps.push(temperatureInKel.toFixed(2) + "K");
+            temps.push(temperatureInCel.toFixed(2) + "\xB0C");
+            temperature = temperatureInCel;
 
-    } else if (userInputTemperatureUnit === "Celsius"){
-        var temperatureInKel = (userInputTemperature) + 273.15;
-        var temperatureInFahr =(userInputTemperature * 9/5) + 32;
+        } else if (userInputTemperatureUnit === "Celsius"){
+            var temperatureInKel = (userInputTemperature) + 273.15;
+            var temperatureInFahr =(userInputTemperature * 9/5) + 32;
 
-        // process data into display format
-        temps.push(userInputTemperature.toFixed(2) + "\xB0C");
-        temps.push(temperatureInKel.toFixed(2) + "K");
-        temps.push(temperatureInFahr.toFixed(2) + "\xB0F");
+            // process data into display format
+            temps.push(userInputTemperature.toFixed(2) + "\xB0C");
+            temps.push(temperatureInKel.toFixed(2) + "K");
+            temps.push(temperatureInFahr.toFixed(2) + "\xB0F");
+            temperature = userInputTemperature;
 
-    } else if (userInputTemperatureUnit === "Kelvin"){
-        var temperatureInCel =(userInputTemperature) - 273.15;
-        var temperatureInFahr =((userInputTemperature - 273.15) * 9/5) + 32;
+        } else if (userInputTemperatureUnit === "Kelvin"){
+            var temperatureInCel =(userInputTemperature) - 273.15;
+            var temperatureInFahr =((userInputTemperature - 273.15) * 9/5) + 32;
 
-        // process data into display format
-        temps.push(userInputTemperature.toFixed(2) + "K");
-        temps.push(temperatureInCel.toFixed(2) + "\xB0C");
-        temps.push(temperatureInFahr.toFixed(2) + "\xB0F");
+            // process data into display format
+            temps.push(userInputTemperature.toFixed(2) + "K");
+            temps.push(temperatureInCel.toFixed(2) + "\xB0C");
+            temps.push(temperatureInFahr.toFixed(2) + "\xB0F");
+            temperature = temperatureInCel;
+        }
 
-    }
+        for (var i =0; i < temps.length; i++) {
+            console.log(temps[i]);
+        }
 
-    for (var i =0; i < temps.length; i++) {
-        console.log(temps[i]);
+        if (temperature <= 0) {
+            console.log("Ooh, it's cold out!");
+            console.log(userInputName + ", I recommend you " + clothes[7] );
+        } else if (temperature > 40) {
+            console.log("Ooh, it's hot out!");
+            console.log(userInputName + ", I recommend you " + clothes[7] );
+        } else if (temperature > 100) {
+            console.log("You are literally boiling!");
+            console.log(userInputName + ", I recommend you " + clothes[7] );
+        } else if (temperature > 0 && temperature < 10) {
+            console.log(userInputName + ", I recommend you wear a " + clothes[6] );
+        } else if (temperature >= 10 && temperature < 20) {
+            console.log(userInputName + ", I recommend you wear a " + clothes[5] );
+        } else if (temperature >= 20 && temperature < 30) {
+            console.log(userInputName + ", I recommend you wear a " + clothes[1] );
+        } else {
+            console.log(userInputName + ", I recommend you wear " + clothes[0] );
+        }
+
+        var userInputContinue = prompt("Do you still want to do more temperature conversions?", "Please enter either Yes or No");
+
+        if (userInputContinue === "Yes") {
+            cont = true;
+        } else {
+            cont = false;
+        }
     }
 }
 
-part3();
+part_3_4_5_6_7();
