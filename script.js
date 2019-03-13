6884362fc94ebe0e2927ce9c371fe9dbeb68cd90
seantanjs <seantanjuisiang@gmail.com>
2019-03-13 21:48:22 +0800

    Completed part 7 of assignment

--- script.js
+++ script.js
@@ -206,6 +206,52 @@ console.log("Assignment1")
 //Part 6
 
 // declare an array variable
+// var temps = [];
+
+// //prompt user for temperature input
+// var temperature = prompt("What is the temperature?");
+
+// //prompt user for temperature unit
+// var temperatureUnit = prompt("What is the unit of temperature? F,C or K?");
+
+// //append degree symbol if temperature unit is either F or C
+// if(temperatureUnit === "F" || temperatureUnit === "C") {
+//     var temperatureUnitWithOrWithoutDegreeSymbol = "°" + temperatureUnit;
+// } else if (temperatureUnit === "K") {
+//     temperatureUnitWithOrWithoutDegreeSymbol = temperatureUnit;
+// }
+
+// //initialize starting temperature based on user input
+// var startingTemperature = temperature + "" + temperatureUnitWithOrWithoutDegreeSymbol;
+
+// //if user temperature input is in F, conversion to C and K will be done
+// if(temperatureUnit === "F") {
+//     var temperatureInCel = (temperature - 32) * (5/9);
+//     var temperatureInKel = ((temperature - 32) * (5/9) + 273.15);
+//     temps.push(startingTemperature, temperatureInCel, temperatureInKel);
+
+// //if user temperature input is in C, conversion to F and K will be done
+// } else if(temperatureUnit === "C") {
+//     var temperatureInFahr = (temperature * (9/5)) + 32 ;
+//     var temperatureInKel = (temperature * 1)+ 273.15 ;
+//     temps.push(startingTemperature, temperatureInFahr, temperatureInKel);
+
+// //if user temperature input is in K, conversion to C and F will be done
+// } else if(temperatureUnit === "K") {
+//     var temperatureInCel = temperature - 273.15;
+//     var temperatureInFahr = (temperature - 273.15) * (9/5) + 32;
+//     temps.push(startingTemperature, temperatureInCel, temperatureInFahr);
+// }
+
+// for(var i=0; i<temps.length; i++) {
+//     console.log(temps[i]);
+// }
+
+//Part 7
+
+var flag = true;
+while(flag) {
+    // declare an array variable
 var temps = [];
 
 //prompt user for temperature input
@@ -245,4 +291,14 @@ if(temperatureUnit === "F") {
 
 for(var i=0; i<temps.length; i++) {
     console.log(temps[i]);
+}
+
+//prompt the user if he/she wants to stop the questioning
+var userInput = prompt("Do you want this to stop?")
+if(userInput === "Yes") {
+    flag = false;
+} else {
+    flag = true;
+}
+
 }
