6907a37020e88e4ae51507414eb8dddadac982f7
Claire Elizabeth Seah <claireseah@Claires-MacBook-Air.local>
2019-03-13 09:05:42 +0800

    added pseudocode to explain what steps I would have taken.

--- temperature_converter.js
+++ temperature_converter.js
@@ -9,9 +9,13 @@ alert("The temperature in celsius is "+temperatureInCelsius+"c."+" "+"While the 
 var temperature = prompt("What is the temperature in (any) degrees?")
 console.log(temperature);
 
-var temperatureInUnit = prompt("Now what unit will this temperature take?")
+var temperatureInUnit = prompt("Now what unit will this temperature take? Choose between Fahrenheit, kelvin, and celsius.")
 console.log(temperatureInUnit);
 
-var 
+/* Apologies, I stayed to try and finish the deliverable but spent too much time trying to work out how to use a for..in loop 
+(which still doesn't work...). I didn't manage to finish this part. I would have used boolean expressions to compare the 
+input from the user and then to use that as the prompt JS to generate the relevant answers depending on that input.
 
-if (temperatureInUnit === 
+Hence, starting by defining the variables for Fahrenheit, kelvin and celsius, using parseInt() to define the numerical values 
+ane produce a mathematical expression. Then using these defined variables in the if, else if, else statement to generate the relevant 
+output. */ 
