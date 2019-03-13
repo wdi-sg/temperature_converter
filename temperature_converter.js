c3083955a90c74ecbef719369ba3df5321078e8a
Claire Elizabeth Seah <claireseah@Claires-MacBook-Air.local>
2019-03-13 11:01:48 +0800

    completed more steps in the exercise

--- temperature_converter.js
+++ temperature_converter.js
@@ -9,13 +9,29 @@ alert("The temperature in celsius is "+temperatureInCelsius+"c."+" "+"While the 
 var temperature = prompt("What is the temperature in (any) degrees?")
 console.log(temperature);
 
-var temperatureInUnit = prompt("Now what unit will this temperature take? Choose between Fahrenheit, kelvin, and celsius.")
-console.log(temperatureInUnit);
+var temperatureUnit = prompt("Now what unit will this temperature take? Choose between Fahrenheit, kelvin, and celsius.");
+console.log(temperatureUnit);
 
-/* Apologies, I stayed to try and finish the deliverable but spent too much time trying to work out how to use a for..in loop 
-(which still doesn't work...). I didn't manage to finish this part. I would have used boolean expressions to compare the 
-input from the user and then to use that as the prompt JS to generate the relevant answers depending on that input.
+var temps = {}
 
-Hence, starting by defining the variables for Fahrenheit, kelvin and celsius, using parseInt() to define the numerical values 
-ane produce a mathematical expression. Then using these defined variables in the if, else if, else statement to generate the relevant 
-output. */ 
+if (temperatureUnit === "Fahrenheit") {
+	temps["Fahrenheit"] = temperature;
+	temps["celsius"] = (temperature - 32) * (5/9);
+	temps["kelvin"] = (temperature - 32) * (5/9) + 273.15;
+} else if (temperatureUnit === "kelvin") {
+	temps["kelvin"] = temperature;
+	temps["Fahrenheit"] = (temperature - 273.15) * (9/5) + 32;
+	temps["celsius"] = temperature - 273.15;  
+} else if (temperatureUnit === "celsius") {
+	temps["celsius"] = temperature;
+	temps["kelvin"] = temperature + 273.15;
+	temps["Fahrenheit"] = (temperature - 273.15) * (9/5) +32;
+ } else {
+ 	alert("Invalid value");
+ };
+
+ console.log(temps);
+
+ for (var key in temps) {
+ 	alert(key + ": " +temps[key]);
+ };
