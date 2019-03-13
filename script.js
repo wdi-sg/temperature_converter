079f09314bf6dfe9e33e9e338ec48ed2f96a75c5
shiowwj <shiowweijun@gmail.com>
2019-03-13 09:08:57 +0800

    Added part 5

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var userInput = prompt("What's your name?")
-console.log(userInput);
+// var userInput = prompt("What's your name?")
+// console.log(userInput);
 
-alert("I'm saying something");
+// alert("I'm saying something");
 
 /*
 1. userinput prompt for temp(inputFahr)
@@ -15,15 +15,16 @@ alert("I'm saying something");
 4. if input2 = kel
     input1=xx
 */
-//part 7 loop
+// part 2:
 
 
-// takes in temp & unit as user prompts
+
 var inputTemp, inputUnit;
 // create output variables
 var outFahr, outCel, outKel;
 //part 3 array
 var temps =[];
+
 var clothes = [
  'nothing',
  'swimsuit',
@@ -34,37 +35,67 @@ var clothes = [
  'heavy jacket and toe warmers',
  'dont go outside if you want to live'
 ];
-
-inputTemp = prompt("What is the Temperature?");
+// prompt to take in temp & unit as user prompts
+inputTemp = parseInt(prompt("What is the Temperature?"));
 console.log(inputTemp);
-inputUnit = prompt("Is this is Fahrenheit, Celsius or Kelvin?");
+inputUnit = prompt("Is this in Fahrenheit, Celsius or Kelvin?");
 console.log(inputUnit);
 
 if (inputUnit ==="Fahr" || inputUnit === "fahr" || inputUnit ==="Fahrenheit" || inputUnit==="f") {
     outCel = (inputTemp - 32) * (5/9);
     outKel = (inputTemp -32) * (5/9) + 273.15;
-    console.log(outCel + " Celsius", outKel + " kelvin");
-} else if (inputUnit ==="Cel" || inputUnit === "celsuis" || inputUnit ==="c" || inputUnit ==="C") {
-    outKel = (inputTemp + 273.15);
+    console.log(inputTemp + "F" + " "+ "=" + " " +outCel.toFixed(2)+ "C" + " " + "=" + " " + outKel.toFixed(2) + "K");
+    console.log(inputUnit);
+    // console.log(outKel);
+    // console.log(outCel);
+    // console.log(outFahr);
+} else if (inputUnit ==="Cel" || inputUnit === "celsius" || inputUnit ==="c" || inputUnit ==="C" || inputUnit === "Celsius") {
     outFahr = (inputTemp * 9/5) + 32;
-    console.log(outKel + " kelvin", outFahr + " F");
+    outKel = inputTemp + 273.15;
+    console.log(inputTemp + "C" + " "+ "=" + " " +outFahr.toFixed(2)+ "F" + " " + "=" + " " + outKel.toFixed(2) + "K");
+    console.log(inputUnit);
+    // console.log(outKel);
+    // console.log(outCel);
+    // console.log(outFahr);
 } else if (inputUnit ==="kel" || inputUnit === "kelvin" || inputUnit ==="k" || inputUnit ==="Kelvin") {
     outFahr = (inputTemp - 273.15) * 9/5 + 32;
     outCel = (inputTemp  - 273.15);
-    console.log(outFahr + " Fahr", outCel + " c");
+    console.log(inputTemp + "K" + " "+ "=" + " " +outFahr.toFixed(2)+ "F" + " " + "=" + " " + outCel.toFixed(2) + "C");
+    console.log(inputUnit);
+    // console.log(outKel);
+    // console.log(outCel);
+    // console.log(outFahr);
 }
 
 
+
 //part 3 & 4
-var temps = [inputTemp, outFahr, outCel, outKel]
-console.log(temps);
+var tempsArr = [inputTemp, outFahr, outCel, outKel]
+// var temps =[];
+console.log(tempsArr);
+// console.log(tempsArr[1] != 0);
+// for (var i = 0; i < temps.length; i++){
+//     if (tempsArr[i] !== 0) {
+//         console.log(tempsArr[i]);
+//         temps.push(tempsArr[i]);
+//         console.log(temps);
+//     }
+// }
+// console.log(temps);
+
+var randomNo = Math.floor(Math.random() * clothes.length);
+console.log(randomNo);
+
+
 console.log(outCel);
-if (outCel <= 0 ) {
-    console.log("ooh it's COLD out");
-} else if ( outCel >= 40 && outCel <= 100 ) {
-    console.log("ooh it's HOT out");
-} else if (outCel >= 100 ) {
-    console.log("your literally boiling");
+if (outCel < 0 || outKel < 273.15) {
+    console.log("ooh it's COLD out, wear some "+ clothes[randomNo]);
+} else if ( (outCel >= 0 && outCel <= 40) || (outKel >= 273.15  && outKel <= 273.15 +40) ) {
+    console.log("Normal weather dude, wear some "+ clothes[randomNo]);
+} else if ( (outCel > 40 && outCel < 100) || (outKel > 273.15 +40 && outKel <273.15 +100) ) {
+    console.log("ooh it's HOT out, wear some "+ clothes[randomNo]);
+} else if (outCel >= 100 || outKel >= 273.15 +100) {
+    console.log("your literally boiling, wear some " + clothes[randomNo]);
 }
 
 //part 5
