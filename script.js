726d0de354d8e92d0846b1d61ef7e2bbd3d7e1d9
riv23 <jleezhenhau@gmail.com>
2019-03-13 09:20:12 +0800

    part 1 and bits of part 2

---
+++ script.js
@@ -0,0 +1,38 @@
+// var temperatureInFahr = prompt("What's your temperature in fahrenheit?");
+// var Kelvin = convertToKelvin(temperatureInFahr)
+// var celsius = convertToCelsius(temperatureInFahr);
+// console.log("The temperature in Fahrenheit is " + temperatureInFahr + " and the temperature in Kelvin is " + Kelvin + "\xB0 and the temperature in celsius is " + celsius);
+
+// function convertToKelvin (fTemp){
+// return (fTemp-32)*5/9+273.15;
+// }
+
+// function convertToCelsius (fTemp){return ((fTemp-32)*5/9);
+// }
+
+
+var temperature = prompt ("What is the temperature now?");
+console.log ("the temperature is " + temperature + "\xB0");
+
+var Fahrenheit = temperature;
+var Celsius = temperature;
+var Kelvin = temperature;
+
+result = prompt ("Please fill in either Fahrenheit, Celsius or Kelvin.");
+
+if (Fahrenheit) {
+    ((temperature + "\xB0 F " = ((temperature-32)*5/9) + "/xB0C" = ((temperature-32)*5/9)+273.15 + "K");
+
+} else if (Celsius){
+    (temperature + "\xB0C " = (temperature + 273.15 + "K") = ((temperature*9/5) ]+ 32) + "\xB0C F";
+
+} else if (Kelvin){
+    (temperature + "K") = (temperature - 273.15 + "\xB0 +C") = (((temperature - 273.15)*9/5)) + 32 + "\xB0 F");
+
+}
+
+
+function temperature(result){
+    if (result = Fahrenheit) {
+        return temperature
+}
