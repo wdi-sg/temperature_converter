696413bd3f56e5a5c353a15c60b2b1451a791797
valenlyn <valenlynchua@gmail.com>
2019-03-13 08:47:42 +0800

    Made changes to JavaScript

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 console.log("Hi!")
 
-var temperatureInFahr = prompt("What is the temperature in fahrenheit?");
+var temperatureInFahr = prompt("What is the temperature?");
 var fahrenheit = temperatureInFahr;
 console.log(temperatureInFahr);
+
+var temperatureInUnit = prompt("What is the temperature's unit of measurement? Type 'fahrenheit'");
+console.log(temperatureInUnit);
 
 var celcius = Math.round(((((fahrenheit - 32)*5)/9)) * 100)/100;
 var kelvin = Math.round(((fahrenheit - 32) * (5/9) + (273.15)) * 100)/100;
@@ -10,17 +13,14 @@ var kelvin = Math.round(((fahrenheit - 32) * (5/9) + (273.15)) * 100)/100;
 function convertFahrenheit (fahrenheit) {
     alert("That equals to " + celcius + "°C, or " + kelvin + "K");
 }
-convertFahrenheit();
+// convertFahrenheit();
 
-// deeeeaddd
-
-// var temperatureInUnit = prompt("What is the temperature's unit?", "Fahrenheit");
-
-// switch(temperatureInUnit) {
-//     case "Fahrenheit":
-//         message = (fahrenheit + "°F" + "is " + celcius + "°C and" + kelvin + "K");
-//             break;
-//             default:
-//                 message = "nothing";
-
-// }
+if (temperatureInUnit == "fahrenheit") {
+    convertFahrenheit();
+} else if (temperatureInUnit == "celcius") {
+        alert("I can't calculate that yet.");
+    } else if (temperatureInUnit == "kelvin") {
+        alert("I can't calculate that yet.");
+        } else {
+             alert("I can't calculate that yet.");
+        }
