b07f52869d23c04ce52f2dcaca4e971d7e7ca57d
hweechieh <hweechieh14@gmail.com>
2019-12-11 01:23:38 +0800

    incomplete attempt on part 02 - changes made to html css and js to add drop down button

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 var inputHappened = function(currentInput) {
     console.log(currentInput);
 
+function clickSelectUnitBtn() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
 //const celciusTemperature = formula used
 //returns an integer
     const celciusTemperature = (currentInput - 32) * (5/9);
@@ -12,7 +16,11 @@ var inputHappened = function(currentInput) {
      var kelvinTemperature = (celciusTemperatureInteger) + 273.15;
      console.log(kelvinTemperature);
 
-    var output = [celciusTemperatureInteger + " Celsius" + ", " + kelvinTemperature + " Kelvin"];
+    var output = [celciusTemperatureInteger + "°C" + ", " + kelvinTemperature + "K"];
     return output;
 }
 inputHappened(70);
+
+// Prompt the user for a starting temperature. This should be a numerical value that represents degrees.
+// Prompt the user for a starting temperatureUnit. This will represent either Fahrenheit, Celsius, or Kelvin.
+// The application should print out the user-submitted temperature in the two remaining units.
