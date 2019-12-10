e47f6b4748a7781887b73b194b73d164095f311e
ShaneC <shanes_email_is@yahoo.com>
2019-12-10 17:49:09 +0800

    done part 1

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 console.log("hello script js");
+// This converts fahrenheit to celsius.
+var calculateCelsius = function(fah) {
+    return (fah - 32) * 5/9;
+}
+
+// This converts fahrenheit to kelvin.
+var calculateKelvin = function(fah) {
+    return (fah - 32) * 5/9 + 273.15;
+}
+
+// // This converts celsius to fahrenheit.
+// var calculateFahrenheit = function(celsius) {
+//     return (celsius * 9/5) + 32;
+// }
+// // This converts celsius to kelvin.
+// var calculateKelvin = function(celsius) {
+//     return (celsius + 273.15);
+// }
+// // This converts kelvin to fahrenheit.
+// var calculateFahrenheit = function(kelvin) {
+//     return (kelvin - 273.15) * 9/5 + 32;
+// }
+// // This converts kelvin to celsius.
+// var calculateCelsius = function(kelvin) {
+//     return (kelvin - 273.15);
+// }
+
+
+
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+  var num = parseInt(currentInput);
+
+  var output = "The temperature is "+calculateKelvin(num)+"K & "+calculateCelsius(num)+"C";
   return output;
 };
