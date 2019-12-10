bb7a83c204f0969001e402d01769f7e4fe35b54e
SafrazHakamali <safrazhakamali@gmail.com>
2019-12-10 20:24:14 +0800

    Working version of further part 2

--- script.js
+++ script.js
@@ -7,20 +7,36 @@ var tempConvert = function() {
 	var x = document.getElementById("tempUnit").selectedIndex;
 	var y = document.getElementById("tempUnit").options;
 	var unit = y[x].text;
-	var farenToCel = Math.round((inputN - 32) * 5/9*100)/100;
-	var farenToKel = Math.round((inputN - 32) * 5/9*100)/100 + 273.15;
-	var celToFaren = "celToFaren";
-	var celToKel = "celToKel";
-	var kelToFaren = "kelToFaren";
-	var kelToCel = "kelToCel";
-	if (unitN = "Farenheit") {
-		var log = farenToCel +"°C = " + farenToKel +"°K";
-	} else if (unitN = "Celcius") {
-		var log = "celToFaren" +"°F = " + "celToKel" +"°K";
-	} else {
-		var log = "kelToFaren" +"°F = " + "kelToCel" +"°C";
+	console.log(unit);
+	var farenToCel = Math.round((inputN - 32) * 5/9*10)/10;
+	var farenToKel = farenToCel + 273.2;
+	var celToFaren = Math.round(inputN*9/5*10)/10+32;
+	var celToKel = parseInt(inputN) + 273.2;
+	var kelToFaren = Math.round((inputN-273.2)*9/5*10)/10+32;
+	var kelToCel = inputN - 273.2;
+	var log;
+	switch (unit) {
+		case "Farenheit":
+		log = farenToCel +"°C = " + farenToKel +"°K"
+		break;
+		case "Celcius":
+		log = celToFaren +"°F = " + celToKel +"°K"
+		break;
+		case "Kelvin":
+		log = kelToFaren +"°F = " + kelToCel +"°C"
+		break;
 	}
-	var output = inputN + "°" + unit +"= "+input;
+
+
+
+	/*if (unit = "Farenheit") {
+		var log = farenToCel +"°C = " + farenToKel +"°K";
+	} else if (unit = "Celcius") {
+		var log = celToFaren +"°F = " + celToKel +"°K";
+	} else {
+		var log = kelToFaren +"°F = " + kelToCel +"°C";
+	}*/
+	var output = inputN + "°" + unit +"= "+log
 	document.getElementById("output").innerHTML = output;
 	return output;
 }
