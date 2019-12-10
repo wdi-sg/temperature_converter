ac2bf9c00c91ae2e0a6cd700953bb1464da79d04
upieez <samuelhww@gmail.com>
2019-12-10 17:14:34 +0800

    split the formula into two different function

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 console.log("hello script js");
 
+var calculateCelsius = function(input) {
+    var celsius = ((input - 32) * 5) / 9;
+    return celsius;
+}
+
+var calculateKelvin = function(input) {
+    var kelvin = (((input - 32) * 5) / 9) + 273.15;
+    return kelvin;
+}
+
 var inputHappened = function(currentInput){
   var output = parseInt (currentInput);
-  var outputCelsius = ((output - 32) * 5) / 9;
-  var outputKelvin = (((output - 32) * 5) / 9) + 273.15;
-  return "Temperature: " + output + " Farenheit, " + outputCelsius + " Celsius, " + outputKelvin + " Kelvin";
+  var celsius = Math.round(calculateCelsius(currentInput) * 100) / 100;
+  var kelvin = Math.round(calculateKelvin(currentInput) * 100) / 100;
+  return "Temperature: " + output + " Farenheit, " + celsius + " Celsius, " + kelvin + " Kelvin";
 };
