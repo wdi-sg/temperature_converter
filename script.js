8c725fcacd597d0a273fb5f260904e524d8ae60b
benjacoblee <benjacoblee@gmail.com>
2019-12-10 22:04:07 +0800

    changed vars to consts, added line-break to suggestClothes return message

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const outputBox = document.querySelector("#output");
 outputBox.innerText = "Enter a temperature"
 
 var inputHappened = function(currentInput) {
-
   if (!gotTemp) {
     temp = parseInt(getTemp(currentInput));
     if (!isNaN(temp)) {
@@ -22,8 +21,7 @@ var inputHappened = function(currentInput) {
   }
   if (gotTemp) {
     gotTemp = false;
-    console.log(gotTemp);
-    let tempUnit = getTempUnit(currentInput).toLowerCase();
+    const tempUnit = getTempUnit(currentInput).toLowerCase();
     if (tempUnit === "fah") {
       return `${fahToKelAndCel(temp)}
       ${printMessage(temp)}
@@ -46,26 +44,26 @@ var getTemp = function(currentInput) {
 };
 
 var getTempUnit = function(currentInput) {
-  var tempUnit = currentInput;
+  const tempUnit = currentInput;
   console.log(temp, typeof temp);
   return tempUnit;
 };
 
 var fahToKelAndCel = function(fah) {
-  var fahToKel = (fah - 32) * (5 / 9) + 273.15;
-  var fahToCel = (fah - 32) * (5 / 9);
+  const fahToKel = (fah - 32) * (5 / 9) + 273.15;
+  const fahToCel = (fah - 32) * (5 / 9);
   return `${fah} F = ${fahToKel.toFixed(2)} K = ${fahToCel.toFixed(2)} C`;
 };
 
 var celToFahAndKel = function(cel) {
-  var celToFah = cel * (9 / 5) + 32;
-  var celToKel = cel + 273.15;
+  const celToFah = cel * (9 / 5) + 32;
+  const celToKel = cel + 273.15;
   return `${cel} C = ${celToFah.toFixed(2)} F = ${celToKel.toFixed(2)} K`;
 };
 
 var kelToCelAndFah = function(kel) {
-  var kelToCel = kel - 273.15;
-  var kelToFah = (kel - 273.15) * (9 / 5) + 32;
+  const kelToCel = kel - 273.15;
+  const kelToFah = (kel - 273.15) * (9 / 5) + 32;
   return `${kel} K = ${kelToCel.toFixed(2)} C = ${kelToFah.toFixed(2)} F`;
 };
 
@@ -82,22 +80,30 @@ var printMessage = function(temp) {
 };
 
 var clothesMessage = function(temp) {
-  var restartMessage = "Enter a number to restart.";
+  const restartMessage = "Enter a number to restart.";
   if (temp < -50) {
-    return `${name}, don't go outside if you want to live. ${restartMessage}`;
+    return `${name}, don't go outside if you want to live. 
+    ${restartMessage}`;
   } else if (temp > -50 && temp <= -20) {
-    return `${name}, wear a heavy jacket and toe warmers. ${restartMessage}`;
+    return `${name}, wear a heavy jacket and toe warmers. 
+    ${restartMessage}`;
   } else if (temp > -20 && temp <= 0) {
-    return `${name}, wear a heavy jacket. ${restartMessage}`;
+    return `${name}, wear a heavy jacket. 
+    ${restartMessage}`;
   } else if (temp > 0 && temp <= 5) {
-    return `${name}, wear a sweather + jacket. ${restartMessage}`;
+    return `${name}, wear a sweather + jacket. 
+    ${restartMessage}`;
   } else if (temp > 5 && temp <= 20) {
-    return `${name}, wear a sweater. ${restartMessage}`;
+    return `${name}, wear a sweater. 
+    ${restartMessage}`;
   } else if (temp > 20 && temp <= 25) {
-    return `${name}, wear shorts and a shit. ${restartMessage}`;
+    return `${name}, wear shorts and a shit. 
+    ${restartMessage}`;
   } else if (temp > 25 && temp <= 30) {
-    return `${name}, wear a swimsuit. ${restartMessage}`;
+    return `${name}, wear a swimsuit. 
+    ${restartMessage}`;
   } else {
-    return `${name}, wear nothing. ${restartMessage}`;
+    return `${name}, wear nothing. 
+    ${restartMessage}`;
   }
 };
