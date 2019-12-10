e92e26d57e7f56f54b42038437ff69bad6101569
weizheng1910 <weizheng1910@gmail.com>
2019-12-10 21:23:10 +0800

    done

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+
+function fromF(inputf){
+  tempInF = parseInt(inputf)
+  tempInC = Math.round((tempInF - 32) * 5 / 9);
+  tempInK = Math.round((tempInF - 32) * 5 / 9 + 273.15);
+  return tempInC+"C or, "+tempInK+"K";
+  
 };
+
+function fromC(inputc){
+  tempInC = parseInt(inputc)
+  tempInF = Math.round((tempInC * 9 / 5) + 32);
+  tempInK = tempInC  + 273.15;
+  return tempInF+"F or, "+tempInK+"K";
+};
+
+function fromK(inputk){
+  tempInK = parseInt(inputk)
+  tempInF = Math.round((tempInK - 273.15) * 9 / 5 + 32);
+  tempInC = Math.round(tempInK - 273.15);
+  return tempInC+"C or, "+tempInF+"F";
+};
+
+function checkWeather(celcius){
+	if(celcius<0){
+		return "ooh, its cold out"
+	} else if(40<celcius<100){
+		return "ooh, its hot out"
+	} else {
+		return "your literally boiling"
+	}
+}
+
+function attire(celcius){
+	console.log(tempInC)
+	console.log(celcius)
+	if(celcius < -10){
+		return "heavy jacket and toe warmers"
+	} else if (0 <= celcius && celcius < 40 ) {
+		return "normal clothing"
+	} else {
+		return "your birthday suit"
+	}
+}
+
+const state = {
+	current : 0,
+	inputName: 0,
+	inputDegrees : 1, 
+	inputType : 2,
+}
+
+console.log(state.current);
