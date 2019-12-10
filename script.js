1e0c15ebc2cdd82139da75a1aab019254549ed25
upieez <samuelhww@gmail.com>
2019-12-10 17:04:33 +0800

    add celsius and kelvin temperature

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+  var output = parseInt (currentInput);
+  var outputCelsius = ((output - 32) * 5) / 9;
+  var outputKelvin = (((output - 32) * 5) / 9) + 273.15;
+  return "Temperature: " + output + " Farenheit, " + outputCelsius + " Celsius, " + outputKelvin + " Kelvin";
 };
+
+var convertCelsius = function(inputHappened) {
+    // Deduct 32, then multiply by 5, then divide by 9
+
+    var output = ((inputHappened - 32) * 5) / 9
+    return output;
+
+}
