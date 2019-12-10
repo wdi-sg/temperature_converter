05860a29f9c50ff0dce8ef3d44371d3561cfbb63
joyce-paul <joyce_paul@hotmail.com>
2019-12-10 21:40:21 +0800

    Completed Part 1 of project

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+/*Goal: Create a program that can convert a temperature in Fahrenheit, Celsius or Kelvin to the other two units.
+
+Specifications
+Part 1
+Your application should...
+Prompt the user for a starting temperature. This should be a numerical value that represents degrees in fahrenheit.
+After receiving input it should show that same temperature in both Kelvin and Celsius.*/
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+
+var inputHappened = function(currentInput) {
+    var fahrenheit = parseInt(currentInput);
+    var celsius = (fahrenheit - 32)/1.8;
+    var kelvin = ((fahrenheit - 32)/1.8) + 273.15;
+    var output = "The temperature is : " + celsius + " celsius" + " and " + kelvin + " kelvin";
   return output;
 };
+
