8bef45046f759cac46a1ec82af313abb788d864f
SafrazHakamali <safrazhakamali@gmail.com>
2019-12-10 18:53:50 +0800

    Testing commit mid way through programming

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
-};
+//alert("Please enter a numerical temperature & temperature unit");
+
+var tempConvert = function() {
+	var inputN = document.getElementById("input").value;
+	var x = document.getElementById("tempUnit").selectedIndex;
+	var y = document.getElementById("tempUnit").options;
+	var unit = y[x].text;
+	var farenToCel = Math.round((inputN - 32) * 5/9*100)/100;
+	var farenToKel = Math.round((inputN - 32) * 5/9*100)/100 + 273.15;
+	var celToFaren = "celToFaren";
+	var celToKel = "celToKel";
+	var kelToFaren = "kelToFaren";
+	var kelToCel = "kelToCel";
+	if (unitN = "Farenheit") {
+		var log = farenToCel +"°C = " + farenToKel +"°K";
+	} else if (unitN = "Celcius") {
+		var log = "celToFaren" +"°F = " + "celToKel" +"°K";
+	} else {
+		var log = "kelToFaren" +"°F = " + "kelToCel" +"°C";
+	}
+	var output = inputN + "°" + unit +"= "+input;
+	document.getElementById("output").innerHTML = output;
+	return output;
+}
