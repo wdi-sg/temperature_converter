70ca8da3e25dd1d686d322c450c5edfd877ca459
rachellesg <rachelle.yeo@outlook.com>
2019-12-10 16:50:50 +0800

    script added to convert fahrenheit into kelvin & celsius

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+  var output;
+  var temperatureKelvin;
+  var temperatureCelsius;
+  if (isNaN(currentInput) === true) {
+    output = "Invalid, only numbers please!";
+  } else {
+    temperatureKelvin = currentInput + 273.15;
+    temperatureCelsius = (currentInput - 32) * 5 / 9;
+    output = "Temperature in Kelvin: " + temperatureKelvin + "\n"  + "Temperature in Celsius: " + temperatureCelsius;
+  }
   return output;
 };
