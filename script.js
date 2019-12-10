02ebf0afec2b4818f9253f97593c2c7f750e85ac
rohai <rohai.sul@gmail.com>
2019-12-11 02:07:08 +0800

    changed to fahrenheit calculations

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+var inputHappened = function( input ) {
+  var fahrenheit = parseInt( input );
+  var Celsius = (fahrenheit-32)/18;
+  var Kelvin = Celsius + 273.15;
+  var output = Celsius + " Celsius and " + Kelvin + " Kelvin";
   return output;
+
+
 };
+console.log("yay");
