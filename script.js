2a0aedce56237644e64f37af626358b51b7c6985
hweechieh <hweechieh14@gmail.com>
2019-12-10 23:16:55 +0800

    completed part 01

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
-};
+//currentInput = fahrenheitTemperature
+var inputHappened = function(currentInput) {
+    console.log(currentInput);
+
+//const celciusTemperature = formula used
+//returns an integer
+    const celciusTemperature = (currentInput - 32) * (5/9);
+    var celciusTemperatureInteger = parseInt(celciusTemperature);
+    console.log(celciusTemperatureInteger);
+
+     var kelvinTemperature = (celciusTemperatureInteger) + 273.15;
+     console.log(kelvinTemperature);
+
+    var output = [celciusTemperatureInteger + " Celsius" + ", " + kelvinTemperature + " Kelvin"];
+    return output;
+}
+inputHappened(70);
