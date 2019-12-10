1989300c1698e375ad3c1c7ed407b9acf4b6f64a
SamTanFL <samsite_tan@hotmail.com>
2019-12-10 23:32:29 +0800

    extended output box and completed p3

--- script.js
+++ script.js
@@ -9,25 +9,44 @@ let tempC1;
 let tempK;
 let unitType;
 let tempNum;
+let tempDesc;
 
-convertTemp= function (tempNum) {
+convertTemp = function (tempNum) {
     if (unitF === true) {
     tempF = tempNum;
     tempC = (tempF - 32) * 5 / 9;
     tempK = (tempC + 273.15);
+    return tempC1;
     } else if (unitC === true) {
         tempC = tempNum;
         tempC1 = parseInt(tempC);
         tempK = (tempC1 + 273);
         tempF = (tempK - 273.15) * 9 / 5 + 32;
+        return tempC1;
     } else if (unitK === true) {
         tempK = tempNum;
         tempC = tempK - 273.15;
         tempF = (tempC * 9 / 5) + 32;
+        return tempC1;
     } else {
         output = "I screwed something up"
+        return output;
     };
 }
+
+tempCheck = function () {
+    if (tempC1 < 0) {
+        tempDesc = "Bbbbrrrrr....";
+    } else if (tempC1 > 40 && tempC1 < 100) {
+        tempDesc = "Sweating Profusely*";
+    } else if (tempC1 >= 100) {
+        tempDesc = "Dies*";
+    } else {
+        tempDesc = "Weather isn't so bad, could be worse."
+    };
+}
+
+
 var inputHappened1 = function(currentInput) {
     console.log(currentInput);
     let unitType = currentInput;
@@ -58,6 +77,7 @@ var inputHappened1 = function(currentInput) {
 var inputHappened2 = function(currentInput){
     tempNum = currentInput;
     convertTemp(tempNum);
-    var output = "The temperature in Celsius is " + tempC + "C." + "\n" + "The temperature in Kelvin is " + tempK + "K." + "\n" + "The temperature in Fahrenheit is " + tempF + "F.";
+    tempCheck();
+    var output = "The temperature in Celsius is " + tempC + "C." + "\n" + "The temperature in Kelvin is " + tempK + "K." + "\n" + "The temperature in Fahrenheit is " + tempF + "F." + "\n" + tempDesc;
   return output;
 };
