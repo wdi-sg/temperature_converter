85bbc47752aa8756e9fc6e1db8694a9b27fb11ca
SamTanFL <samsite_tan@hotmail.com>
2019-12-10 17:31:09 +0800

    added logic and styles for P1

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 console.log("hello script js");
 
+/*
+C to F
+Celsius = (Fahrenheit - 32) * 5 / 9.
+F to C
+Fahrenheit = (Celsius * 9) + 32.
+K to C
+Kelvin = Celsius + 273.15.
+C to K
+Celsius = Kelvin - 273.15.
+*/
+
+let tempF;
+let tempC;
+let tempK;
+
+convertFtoC = function (currentInput) {
+    tempF = currentInput;
+    tempC = (tempF - 32) * 5 / 9;
+    tempK = (tempC + 273.15);
+    return tempC, tempK;
+}
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+    convertFtoC(currentInput);
+    var output = "The temperature in Celsius is " + tempC + "C. The temperature in Kelvin is " + tempK + "K.";
   return output;
 };
+
+/*var inputHappened = function(currentInput){
+  console.log( currentInput );
+  var output = "";
+  return output;
+};*/
