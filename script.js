b0ffef2d5416b8b50df1e3ddd09572d0a69b0737
Robert Kolsek <kolsek.income@gmail.com>
2019-12-11 06:33:21 +0800

    changed if else cluster to switch statements

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let unit
 
 document.querySelector('#input').addEventListener('change', function (event) {
   var currentInput = event.target.value;
-  var output = inputHappened(currentInput, undefined);
+  var output = inputHappened(currentInput);
   display(output);
 });
 
@@ -21,6 +21,7 @@ var inputHappened = function (currentInput) {
     degree = parseInt(currentInput)
     inputAmount++
     document.querySelector('#input').setAttribute("placeholder", "Celsius/Fahrenheit/Kelvin")
+
     if (currentInput < 0) {
       return `Oooooh it might be cold out!`
     } else if (currentInput > 40 && currentInput <= 100) {
@@ -30,10 +31,12 @@ var inputHappened = function (currentInput) {
     } else {
       return `seems like it could be a nice day`
     }
+
     // second input
   } else if (inputAmount === 1) {
     unit = currentInput
     document.querySelector('#input').setAttribute("placeholder", "Your Name please")
+
     if (currentInput === "Celsius") {
       let output = `${degree}°C = ${9/5*degree+32}°F = ${degree + 273}°K`
       inputAmount++
@@ -47,33 +50,43 @@ var inputHappened = function (currentInput) {
       inputAmount++
       return output
     }
+
     // third input
   } else if (inputAmount === 2) {
+    inputAmount = 0
+    document.querySelector('#input').setAttribute("placeholder", "input temperature!")
+
     if (degree < 0) {
       return `${currentInput}, please dont go outside if you want to live`
+
     } else if (degree > 40 && degree <= 100) {
-      if (unit === "Celsius") {
-        return `${currentInput}, you should wear swimwear, or maybe nothing, or maybe, just maybe, enter a cold storage`
-      } else if (unit === "Fahrenheit") {
-        return `${currentInput}, could be cold, could be hot. Toughen up, buttercup!`
-      } else if (unit === "Kelvin") {
-        return `${currentInput}, let's just say.... it is cold`
+      switch (unit) {
+        case "Celsius":
+          return `${currentInput}, you should wear swimwear, or maybe nothing, or maybe, just maybe, enter a cold storage`
+        case "Fahrenheit":
+          return `${currentInput}, could be cold, could be hot. Toughen up, buttercup!`
+        case "Kelvin":
+          return `${currentInput}, let's just say.... it is cold`
       }
+
     } else if (degree > 100) {
-      if (unit === "Celsius") {
-        return `${currentInput}, you should wear swimwear, or maybe nothing, or maybe, just maybe, enter a cold storage`
-      } else if (unit === "Fahrenheit") {
-        return `${currentInput}, either you melt, or you die. Show your skin`
-      } else if (unit === "Kelvin") {
-        return `${currentInput}, let's just say.... it is cold, or very hot`
+      switch (unit) {
+        case "Celsius":
+          return `${currentInput}, you should wear swimwear, or maybe nothing, or maybe, just maybe, enter a cold storage`
+        case "Fahrenheit":
+          return `${currentInput}, either you melt, or you die. Show your skin`
+        case "Kelvin":
+          return `${currentInput}, let's just say.... it is cold, or very hot`
       }
+
     } else {
-      if (unit === "Celsius") {
-        return `${currentInput}, most likely you will need to wear a sweater`
-      } else if (unit === "Fahrenheit") {
-        return `${currentInput}, do you want to build a snowmaaaaannn?  `
-      } else if (unit === "Kelvin") {
-        return `${currentInput}, human extinction is on the way.`
+      switch (unit) {
+        case "Celsius":
+          return `${currentInput}, most likely you will need to wear a sweater`
+        case "Fahrenheit":
+          return `${currentInput}, do you want to build a snowmaaaaannn?`
+        case "Kelvin":
+          return `${currentInput}, human extinction is on the way.`
       }
     }
   }
