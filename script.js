16bcd2b784b2d01b60273c9daf5fdc2ef7d124c2
upieez <samuelhww@gmail.com>
2019-12-10 18:23:57 +0800

    add temperature statement

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-console.log("hello script js");
 
-var calculateCelsius = function(input) {
+var farenheitToCelsius = function(input) {
     var celsius = ((input - 32) * 5) / 9;
     return celsius;
-}
+};
 
-var calculateKelvin = function(input) {
+var farenheitToKelvin = function(input) {
     var kelvin = (((input - 32) * 5) / 9) + 273.15;
     return kelvin;
-}
+};
+
+var celsiusToFarenheit = function(input) {
+    var farenheit = (((input * 9) / 5) + 32);
+    return farenheit;
+};
+
+var kelvinToFarenheit = function(input) {
+    var kelvin = (((input - 273.15)* 9) / 5) + 32;
+    return kelvin;
+};
+
+var statementWeather = function(input) {
+    if (input < 0) {
+        return "ooh it's cold out";
+    } else if (input > 40 && input < 100) {
+        return "ooh it's hot out";
+    } else if (input > 100) {
+        return "you're literally boiling";
+    } else {
+        return "";
+    }
+};
 
 var inputHappened = function(currentInput){
-  var output = parseInt (currentInput);
-  var celsius = Math.round(calculateCelsius(currentInput) * 100) / 100;
-  var kelvin = Math.round(calculateKelvin(currentInput) * 100) / 100;
-  return "Temperature: " + output + " Farenheit, " + celsius + " Celsius, " + kelvin + " Kelvin";
+  var output = currentInput;
+  var celsius = Math.round(farenheitToCelsius(currentInput) * 100) / 100;
+  var kelvin = Math.round(farenheitToKelvin(currentInput) * 100) / 100;
+  // var celsiusToFarenheit = Math.round(celsiusToFarenheit(currentInput) * 100) / 100;
+  // var kelvinToFarenheit = Math.round(kelvinToFarenheit(currentInput) * 100) / 100;
+  var statement = statementWeather(currentInput);
+  console.log(typeof output);
+  if (output) {
+    return "Temperature: " + output + " Farenheit, " + celsius + " Celsius, " + kelvin + " Kelvin" + " " + statement;
+  }
 };
