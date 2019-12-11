ff08dd76baee23947f24e9489f4210cbf1fc55dd
joyce-paul <joyce_paul@hotmail.com>
2019-12-11 08:50:25 +0800

    amended script.js file

--- script.js
+++ script.js
@@ -7,12 +7,49 @@ Prompt the user for a starting temperature. This should be a numerical value tha
 After receiving input it should show that same temperature in both Kelvin and Celsius.*/
 console.log("hello script js");
 
-
 var inputHappened = function(currentInput) {
     var fahrenheit = parseInt(currentInput);
     var celsius = (fahrenheit - 32)/1.8;
     var kelvin = ((fahrenheit - 32)/1.8) + 273.15;
+
+    // Round down the Fahrenheit temperature
+    fahrenheit = Math.floor(fahrenheit);
+
+    // Round down kelvin
+    kelvin = Math.floor(kelvin);
+
+    // Round down celsius temperature
+    celsius = Math.floor(celsius);
+
     var output = "The temperature is : " + celsius + " celsius" + " and " + kelvin + " kelvin";
   return output;
 };
+
+
+
+
+
+/*!NaN*
+
+ if (currentInput === integer ) {
+    alert("Here is your temperature");
+  } else {
+    alert("This is not a number");
+  };
+
+
+/*Part 2*/
+
+
+
+/*Part 3   and Part 4
+if (temperature > 0) {
+    console.log("ooh it is cold out" + " do not go out if you want to live");
+} else if (temperature is > 40) {
+    console.log("ooh it is hot out" + " wear shorts and shirt");
+} else (temperature > 100) {
+    console.log("you are literally boiling" + " wear a swimsuit");
+}*/
+
+
 
