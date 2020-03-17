d2dec6a066859f4caa2ce3cf91982788e296a52e
travisenquiry <travisenquiry@gmail.com>
2020-03-17 18:47:46 +0800

    done part 1

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+  var input = parseInt(currentInput);
+
+  var kelvin = (input - 32) * 5 / 9 + 273.15;
+  var celsius = (input - 32) * 5 / 9;
+  return `Kelvin: ${kelvin}K, Celsius: ${celsius}C`;
+
 };
