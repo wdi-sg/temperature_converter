560d18a904b9e1d26af9e1426769b2b347fb35ca
kokchuantan <kokchuan1996@gmail.com>
2020-03-17 17:35:13 +0800

    change fahrenheit to celcius and kelvin

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+    if (isNaN(parseInt(currentInput))===false){
+    //fahrenheit to celcius
+  var degreeCelcius = (currentInput - 32) * 5 / 9 ;
+  //fahrenheit to kelvin
+  var degreeKelvin = degreeCelcius + 273 ;
+  return output = currentInput + ' Fahrenheit = ' +(degreeCelcius + 'C  Or ') + (degreeKelvin + 'K');
+    }
 };
