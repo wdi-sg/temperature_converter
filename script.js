f577e4a0869c38cf921a583a73e451a9c4845d14
ThomasOh92 <ohcsthomas@gmail.com>
2020-03-17 23:49:29 +0800

    Functionality for Part 2 of the Exercise

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
 console.log("hello script js");
 
-document.querySelector('#output').innerText = "Please provide a temperature in Fahrenheit"
+document.querySelector('#output').innerText = "Please provide starting temperature"
+
+var status = "getting temperature";
+var temperature;
+var temperatureUnit;
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  if (isNaN(parseInt(currentInput))){
+
+  //Initial State - getting temperature, check if integer
+  if (status === "getting temperature" && isNaN(parseInt(currentInput))){
     var output = "Please provide a number";
     return output;
-  } else {
-    var celsiusConversion = (currentInput - 32) / 1.8;
-    var output = currentInput + " Degrees Fahrenheit is " + celsiusConversion + " Degrees Celsius";
+  }
+
+  //getting temperature, if integer
+  if (status === "getting temperature" &&
+             typeof parseInt(currentInput) === "number"){
+    temperature = parseInt(currentInput);
+    status = "getting temperature unit";
+    var output = "Please provide a temperature unit";
+    return output;
+  }
+
+//verifying temperature unit
+  var tempUnitCorrect = false;
+  if (status === "getting temperature unit" && (
+      currentInput.toLowerCase().includes("fahrenheit") ||
+         currentInput.toLowerCase().includes("celsius") ||
+             currentInput.toLowerCase().includes("kelvin"))){
+    tempUnitCorrect = true;
+    temperatureUnit = currentInput;
+  }
+
+  //getting temp unit, if correct
+  if(status === "getting temperature unit" && tempUnitCorrect){
+    var output;
+    if (temperatureUnit.includes("fahrenheit")){
+        var celsius = (temperature - 32) / 1.8;
+        var kelvin = celsius + 273.15;
+        output = `${temperature} Fahrenheit = ${celsius} Celsius = ${kelvin} Kelvin`;
+    } else if (temperatureUnit.includes("celsius")){
+        var fahrenheit = (temperature * 1.8) + 32;
+        var kelvin = temperature + 273.15;
+        output =`${temperature} Celsius = ${fahrenheit} Fahrenheit = ${kelvin} Kelvin`;
+    } else {
+        var celsius = temperature - 273.15;
+        var fahrenheit = (celsius - 32) / 1.8;
+        output = `${temperature} Kelvin = ${fahrenheit} Fahrenheit = ${celsius} Celsius`;
+    }
+    return output;
+  }
+  //reprompt if temp unit is wrong
+  if (status === "getting temperature unit" && !tempUnitCorrect){
+    var output = "Please choose: fahrenheit, celsius or kelvin";
     return output;
   }
 };
