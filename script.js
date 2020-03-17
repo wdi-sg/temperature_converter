f9d2f58322fc9978a6ca2870a85d56e563cc5b1d
Kenan <xinkenan.11@hotmail.com>
2020-03-17 21:39:01 +0800

    added part1 solution

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-console.log("hello script js");
+const INVALID_INPUT_ERR_MSG = "Please the temperature in Fahrenheit.";
+const isNumericalInput = input => !isNaN(parseInt(input));
+const FtoC = tempInF => (tempInF - 32) * 5 / 9;
+const FtoK = tempInF => 273.5 + ((tempInF - 32) * (5/9));
+const convertTemp = (temp, formula) => formula(temp);
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+const inputHappened = function(currentInput) {
+  const tempInF = currentInput;
+
+  let msg = null;
+  let tempInC = null;
+  let tempInK = null;
+
+  if (!isNumericalInput(currentInput)) return INVALID_INPUT_ERR_MSG;
+
+  tempInC = convertTemp(currentInput, FtoC);
+  tempInK = convertTemp(currentInput, FtoK);
+
+  msg =  `${tempInF} \xB0F is ${tempInC} \xB0C\n`;
+  msg += `${tempInF} \xB0F is ${tempInK} \xB0K`;
+
+  return msg;
 };
