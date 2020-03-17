c395dfe2fbeef591c9ef9b725fb9d1321cb37209
kwansing14 <kwansing@hotmail.sg>
2020-03-18 00:30:22 +0800

    completed part 1-4

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 console.log("hello script js");
 
-/*var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
-};*/
-
 //Part 1: display Kelvin and Celsius
-var inputHappened = function (currentInput) {
+/*var inputHappened = function (currentInput) {
 
     if (isNaN(parseFloat(currentInput))==false)  {
 
@@ -15,5 +9,89 @@ var inputHappened = function (currentInput) {
     var output2 = "Celsius: " + ((currentInput-32)*5/9).toFixed(2) + "C";
 
     return output + output2;
+    }
+}*/
+
+//Part 2: display Kelvin and Celsius
+
+var temp = [];
+var i;
+document.getElementById("input").placeholder = "temperature";
+var inputHappened = function (currentInput) {
+
+    temp.push(currentInput);
+
+    if(temp.length == 1){
+
+        document.getElementById("input").value = "";
+        document.getElementById("input").placeholder = "temperature unit";
+    }
+    if(temp.length == 2) {
+
+        var temperature = temp [0];
+        var temperatureUnit = temp [1];
+
+        if (temperatureUnit == "kelvin"){
+
+            var tempKelvin = temperature;
+            var tempCelsius = (parseFloat(temperature)-273.15).toFixed(2);
+            var tempFahrenheit = ((parseFloat(temperature)-273.15)*9/5+32).toFixed(2);
+
+        } else if (temperatureUnit == "fahrenheit"){
+
+            var tempKelvin = (((parseFloat(temperature)-32)*5/9)+273.15).toFixed(2);
+            var tempCelsius = ((parseFloat(temperature)-32)*5/9).toFixed(2);
+            var tempFahrenheit = temperature;
+
+        } else if (temperatureUnit == "celsius") {
+
+            var tempCelsius = temperature;
+            var tempKelvin = (parseFloat(temperature)+273.15);
+            var tempFahrenheit = ((parseFloat(temperature)*9/5)+32).toFixed(2);
+        }
+
+    //Part 3 Temperature check
+    if(temperature<40) {
+        var tempMsg = "Ooh it's cold out.";
+    } else if (temperature>=40 && temperature<=100) {
+        var tempMsg = "Ooh it's hot out.";
+    } else if (temperature>100) {
+        var tempMsg = "You're literally boiling.";
+    }
+
+    document.getElementById("input").value = "";
+    document.getElementById("input").placeholder = "";
+
+    //Part 4 Prompt Name and clothing
+    var personName = prompt("Please enter your name");
+    var wear;
+    switch (true) {
+        case (tempCelsius>70):
+            wear = "nothing";
+            break;
+        case (tempCelsius>50):
+            wear = "swimsuit";
+            break;
+        case (tempCelsius>30):
+            wear = "shorts and shirt";
+            break;
+        case (tempCelsius>20):
+            wear = "sweater";
+            break;
+        case (tempCelsius>10):
+            wear = "sweater + jacket";
+            break;
+        case (tempCelsius>0):
+            wear = "heavy jacket"
+            break;
+        case (tempCelsius<0):
+            wear = "heavy jacket + toe warmers"
+            break;
+        default:
+            wear = null;
+    }
+
+    return tempFahrenheit+"F = "+tempCelsius+"C = "+tempKelvin+" K\n"
+    +tempMsg+"\n"+personName+", you better be wearing "+wear;
     }
 }
