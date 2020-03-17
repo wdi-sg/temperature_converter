4df618757163fa20b530836536380f4d95d3a288
ThomasOh92 <ohcsthomas@gmail.com>
2020-03-17 17:22:57 +0800

    Functionality for Part 1 of the Exercise

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 console.log("hello script js");
 
+document.querySelector('#output').innerText = "Please provide a temperature in Fahrenheit"
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+  if (isNaN(parseInt(currentInput))){
+    var output = "Please provide a number";
+    return output;
+  } else {
+    var celsiusConversion = (currentInput - 32) / 1.8;
+    var output = currentInput + " Degrees Fahrenheit is " + celsiusConversion + " Degrees Celsius";
+    return output;
+  }
 };
