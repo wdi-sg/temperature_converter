2c205ecc690b9a6c8c90f5342418e51d2528430e
lekhweemeng <lek.hweemeng@live.com>
2020-03-17 22:31:32 +0800

    completed part 3. added logic for the extra verbatim.

--- script.js
+++ script.js
@@ -1,30 +1,62 @@
 var inputHappened = function(currentInput){
     var dropDownValue = document.getElementById("unit");
     var selectedUnit = dropDownValue.options[dropDownValue.selectedIndex].text;
+
 if (selectedUnit === "Fahrenheit"){
   var inKelvin = (((parseInt(currentInput))+459.67)*(5/9));
   console.log(inKelvin)
   var inCelcius = (((parseInt(currentInput)) - 32)*(5/9));
   console.log(inCelcius);
-
-  var output = "Converted to Kelvin is: " + inKelvin + " and converted to celcius is: " + inCelcius;
-  return output;
+  if(inCelcius<0){
+  var output = "Converted to Kelvin is: " + inKelvin + " and converted to celcius is: " + inCelcius + " ooh it's cold out";
+    return output;
+}else if (inCelcius>40 && inCelcius<100){
+ var output = "Converted to Kelvin is: " + inKelvin + " and converted to celcius is: " + inCelcius + " ooh it's hot out";
+    return output;
+} else if (inCelcius>=100)
+{
+     var output = "Converted to Kelvin is: " + inKelvin + " and converted to celcius is: " + inCelcius + " your literally boiling";
+    return output;
+}else {
+    var output = "Converted to Kelvin is: " + inKelvin + " and converted to celcius is: " + inCelcius;
+    return output;
+}
 }else if (selectedUnit === "Celcius"){
   var cToK = ((parseInt(currentInput))+273.15);
   console.log(cToK);
   var cToF = ((parseInt(currentInput))*(9/5)+32);
   console.log(cToF);
-
-  var output = "Converted to Kelvin is: " + cToK + " and converted to Fahrenheit is: " + cToF;
-  return output;
+if ((parseInt(currentInput))<0){
+    var output = "Converted to Kelvin is: " + cToK + " and converted to celcius is: " + cToF + " ooh it's cold out";
+    return output;
+}else if ((parseInt(currentInput))>40 && (parseInt(currentInput))<100){
+var output = "Converted to Kelvin is: " + cToK + " and converted to celcius is: " + cToF + " ooh it's hot out";
+    return output;
+}else if((parseInt(currentInput))>=100){
+    var output = "Converted to Kelvin is: " + cToK + " and converted to celcius is: " + cToF + " your literally boiling";
+    return output;
+}else{
+    var output = "Converted to Kelvin is: " + cToK + " and converted to celcius is: " + cToF;
+    return output;
+}
 }else {
   var kToC = ((parseInt(currentInput))-273.15);
   console.log(kToC);
   var kToF = ((parseInt(currentInput))*(9/5)-459.67);
   console.log(kToF);
-
-  var output = "Converted to Kelvin is: " + kToC + " and converted to Fahrenheit is: " + kToF;
+if(kToC<0){
+  var output = "Converted to Celcius is: " + kToC + " and converted to Fahrenheit is: " + kToF + " ooh it's cold out";
   return output;
+}else if(kToC>40 && kToC<100){
+  var output = "Converted to Celcius is: " + kToC + " and converted to Fahrenheit is: " + kToF + " ooh it's hot out";
+  return output;
+}else if (kToC>=100){
+  var output = "Converted to Celcius is: " + kToC + " and converted to Fahrenheit is: " + kToF + " your literally boiling";
+  return output;
+}else {
+      var output = "Converted to Celcius is: " + kToC + " and converted to Fahrenheit is: " + kToF;
+  return output;
+}
 }
 };
 
