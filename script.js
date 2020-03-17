2d08ed2296633450086c5e7474187ef07f8bbe28
lekhweemeng <lek.hweemeng@live.com>
2020-03-17 17:42:49 +0800

    added in the correct script for k & c calculation

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 console.log("hello script js");
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+  var inKelvin = (((parseInt(currentInput))+459.67)*(5/9));
+  console.log(inKelvin)
+  var inCelcius = (((parseInt(currentInput)) - 32)*(5/9));
+  console.log(inCelcius);
+  var output = "Converted to Kelvin is: " + inKelvin + " and converted to celcius is: " + inCelcius;
   return output;
 };
