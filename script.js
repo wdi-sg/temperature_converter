08aa78c316989dc34d6d4689cc8965ae2e37502e
chelsejw <chelsejw@gmail.com>
2020-03-17 19:59:51 +0800

    add part 3 functionality, add function to clear inputs.

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ console.log("hello script js");
 //Create new array to store user input values.
 var userInput = [];
 
+function clearInput() {
+  return document.getElementById("input").value = ""
+}
+
 //Give starting instructions.
 document.querySelector('#output').innerText = `Hi! To get started, please give me the value of the temperature you wish to convert. (E.g. for 90 Celsius, just type 90. Then, press enter or click anywhere outside the input box.`;
 
@@ -12,20 +16,33 @@ var inputHappened = function(currentInput){
 
 //If user has input only the first value, prompt for second value.
   if (userInput.length===1) {
+    clearInput();
     return output = `Nice! The value you gave is ${userInput[0]}. Next, what's the unit of the temperature you have given? Please input one of the three: C (for Celsius), K (for Kelvin) or F (for Fahrenheit). Then, press enter or click anywhere outside the input box.`
 
 //Once user has given both value and units, convert value appropriately.
   } else if (userInput.length===2){
+    clearInput();
     var temp = parseInt(userInput[0]);
-    var unit = userInput[1];
 
+    //Part 3: Give additional message according to tempature.
+    var msg = "";
+    if (temp < 0) {
+      msg = `Damn it's cold.`
+    } else if (temp > 100) {
+      msg = `Damn it's hot.`
+    } else if (temp > 40) {
+      msg = `Eh it's pretty warm.`
+    } else {
+      msg = `That's an alright temperature.`
+    }
     //Determining what to calculate based on the unit given.
+    var unit = userInput[1];
     if (unit==="C") {
-     output = `${temp}C is.... ${(temp + 273.15).toFixed(1)}K and ${(temp*(9/5) + 32).toFixed(1)}F. Refresh to calculate a new value!`
+     output = `${temp}C is.... ${(temp + 273.15).toFixed(1)}K and ${(temp*(9/5) + 32).toFixed(1)}F. ${msg} Refresh to calculate a new value!`
     } else if (unit==="F") {
-     output = `${temp}F is.... ${(temp*(5/9) + 273.15).toFixed(1)}K and ${((temp -32)*(5/9)).toFixed(1)}C. Refresh to calculate a new value!`
+     output = `${temp}F is.... ${(temp*(5/9) + 273.15).toFixed(1)}K and ${((temp -32)*(5/9)).toFixed(1)}C. ${msg} Refresh to calculate a new value!`
     } else if (unit==="K") {
-     output = `${temp}K is.... ${(temp - 273.15).toFixed(1)}C and ${((temp -273.15)*(9/5) + 32).toFixed(1)}F. Refresh to calculate a new value!`
+     output = `${temp}K is.... ${(temp - 273.15).toFixed(1)}C and ${((temp -273.15)*(9/5) + 32).toFixed(1)}F. ${msg} Refresh to calculate a new value!`
     } else {
       //There was an error. Reset values and give error message.
       userInput = [];
@@ -33,6 +50,7 @@ var inputHappened = function(currentInput){
     }
 //If user has given more than 3 inputs, reset values and give error message.
   } else {
+    clearInput();
   userInput = [];
    output = "Sorry, there was an error. Please ensure your first input is a number and your second input is one of the following three units: C, F or K. Try again by inputting the value of your temperature!"
  }
