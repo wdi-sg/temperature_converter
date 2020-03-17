5f523a68961691df36767f9c54032ea69b39a86a
Rachelik <rachel.ik.87@gmail.com>
2020-03-17 21:30:46 +0800

    add degF conversion

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+//prompt for input
+alert("Please input a temperature in degree fahrenheit.");
+
+
+//Get input in degF and convert to degC and K
+var inputHappened = function(currentInput) {
+  console.log( parseInt(currentInput) );
+  var fahrenheit = parseInt(currentInput);
+  var output = Math.floor((fahrenheit - 32) * 5/9);
+  var outputK = Math.floor(output + 273.15);
+  return currentInput + " degF = " + output + " degC or " + outputK + " K";
 };
