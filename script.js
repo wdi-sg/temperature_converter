2036b0403102364c139ed8abb318128f3e46cf32
travisenquiry <travisenquiry@gmail.com>
2020-03-17 19:22:46 +0800

    done part 3

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ console.log("hello script js");
 var inputHappened = function(currentInput){
   console.log( currentInput );
   var input = parseInt(currentInput);
-
   var kelvin = (input - 32) * 5 / 9 + 273.15;
   var celsius = (input - 32) * 5 / 9;
   return `Kelvin: ${kelvin}K, Celsius: ${celsius}C`;
+
+};
+
+var inputConvert = function(currentInput){
+  console.log( currentInput );
+  return parseInt(currentInput);
 
 };
