c7a5ad8abbeec28ea00a2b7b0d1a2d855d75fd40
wickker <choydianchun@gmail.com>
2020-03-17 21:22:30 +0800

    completed part 1234

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
+/*
+var inputHappened = function(currentInput) {
+  console.log(currentInput);
   var output = "WOW TEMPERATURE";
   return output;
 };
+*/
+
+var inputHappened = function(currentInputF) {
+  let output; 
+  let tempK;
+  let tempC;
+  let tempF = parseInt(currentInputF);
+  if (isNaN(parseInt(currentInputF))) {
+    output = "please enter a NUMBER"
+    return output;
+  }
+  else {
+    tempK = tempF + 241.2;
+    tempC = currentInputF - 32;
+    output = currentInputF + " F = " + tempC + " C = " + tempK + " K";
+    return output;
+  }
+}
