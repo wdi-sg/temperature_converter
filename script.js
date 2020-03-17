e7c20312d07e43fee496fa581f002f94ea4567a1
Ianshalom <you@example.comianshalom92@gmail.comgit config --global user.email you@example.comianshalom92@gmail.com>
2020-03-17 20:47:40 +0800

    commit for part1 script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-console.log("hello script js");
-
+//console.log("hello script js");
+/*
 var inputHappened = function(currentInput){
   console.log( currentInput );
   var output = "WOW TEMPERATURE";
   return output;
 };
+*/
+
+/*
+var inputHappened = function(currentInput){
+  console.log( currentInput );
+var parsed = parseInt(currentInput);
+if (isNaN(parsed) === false) {
+      var output = (parsed - 32) * (5/9);
+      var kelv = (parsed - 32) * (5/9) + 273.15;
+    return output + " Cel and " + kelv + " Kel.";
+} else {
+    return "Please input a number.";
+}
+}
+*/
