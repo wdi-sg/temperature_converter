67ee347fbe30749e30da5e00e108778f1beb28b2
osbornechan <osbornecab@gmail.com>
2020-03-17 17:43:57 +0800

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 console.log("hello script js");
 
+var alertFunction = function () {alert('Please input a temperature below in Farenheit.');
+}
+
+alertFunction();
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+    var notInteger = isNaN(parseInt(currentInput))
+
+    if (!notInteger) {
+        var celsius = (currentInput - 32)*(5/9);
+        var kelvin = celsius + 273.15;
+        var output = 'The temperature is ' + celsius + 'C ' + 'or ' + kelvin + 'K';
+    } else {
+        return "Please enter a valid number."
+    }
+    return output;
 };
