f07972ab46bb805c2824a01dc4054b3deebbc160
elisaes <alisa260785@gmail.com>
2020-03-18 02:16:27 +0800

    temperature converter

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-console.log("hello script js");
+let tempNumber;
+let tempUnit;
+let name;
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
-};
+var inputHappened = function(currentInput) {
+  if (currentInput == parseInt(currentInput)) {
+    tempNumber = currentInput;
+    console.log(tempNumber);
+    return "Please insert unit F (fahrenheit), K (kelvin), C (celcius)";
+  } else {
+    tempUnit = currentInput;
+      if (tempUnit === "F"){
+      celcius = ((tempNumber - 32) * 5) / 9;
+      console.log(celcius);
+      kelvin = celcius + 273;
+            if (celcius < 0) {
+        return celcius + " C " + kelvin + " K " + "ooh it's cold out." + name;
+    }   else if (celcius < 40) {
+        return celcius + " C " + kelvin + " K " + "ooh it's hot out." + name;
+    }   else if (celcius < 100) {
+        return celcius + " C " + kelvin + " K " + "your literally boiling" + name;
+    }}  else if (tempUnit === "C") {
+      fahrenheit = (tempNumber * 9) / 5 + 32;
+      kelvin = tempNumber + 273;
+         if ( tempUnit < 0) {
+        return kelvin + " K " + fahrenheit + " F " + "ooh it's cold out.";
+    }  else if (tempUnit < 40) {
+        return kelvin + " K " + fahrenheit + " F " + "ooh it's hot out.";
+    }  else if (tempUnit < 100) {
+        return kelvin + " K " + fahrenheit + " F " + "your literally boiling";
+    } 
+  } else if (tempUnit === "K") {
+    celcius = tempNumber - 273;
+    fahrenheit = (celcius * 9) / 5 + 32;
+    if (celcius < 0) {
+      return celcius + " C " + fahrenheit + " F " + "ooh it's cold out.";
+    } else if (celcius < 40) {
+      return celcius + " C " + fahrenheit + " F " + "ooh it's hot out.";
+    } else if (celcius < 100) {
+      return celcius + " C " + fahrenheit + " F " + "your literally boiling";
+    }}}
+      
+    }
