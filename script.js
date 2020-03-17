64477774b0d55ee4ba77ca40999e9fa50299c052
marctanyh <marctanyh@gmail.com>
2020-03-18 00:57:35 +0800

    Part 1
    .js created to convert Fahrenhiet to Celsius and Kelvin

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
-};
+//var inputHappened = function(currentInput){
+//  console.log( currentInput );
+//  var output = "WOW TEMPERATURE";
+//  return output;
+//};
+
+var inputHappened = function (currentInput) {
+
+    var tempratureFahrenheit = parseInt ( currentInput );
+
+    var tempratureCelsius = ( ( tempratureFahrenheit - 32 ) * ( 5 / 9 ) );
+
+    var tempratureKelvin = tempratureCelsius + 237.15
+
+    var output = ( tempratureFahrenheit + " Fahrenheit converts to " + tempratureCelsius + " Celsius or " + tempratureKelvin +   " Kelvin");
+
+    return output;
+
+}
