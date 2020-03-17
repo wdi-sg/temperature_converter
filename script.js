44949945e2d751f1ba808de54633d54b57c47221
kokchuantan <kokchuan1996@gmail.com>
2020-03-17 21:07:38 +0800

    part 2 cannot get output to print final message

--- script.js
+++ script.js
@@ -1,11 +1,76 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-    if (isNaN(parseInt(currentInput))===false){
+var temp;
+var type = false;
+var typename;
+
+var inputHappened = function(currentInput) {
+    if(!type) {
+    //check for valid number
+        if (parseFloat(currentInput)) {
+            console.log('test1');
+            temp = parseFloat(currentInput) ;
+            console.log('test2');
+            if (parseFloat(temp)) {
+            console.log('test3');
+            type = true;
+            return output = 'Please choose type of measurement of temperature in cel , fah or kel.';
+            }
+        }
+        else {
+            return output = 'Please enter valid number.';
+        }
+    }
+    if(type){
+        typename = currentInput;
+        switch(typename) {
+
+            case 'cel' :
+            changeCelcius(temp);
+            console.log('test4');
+            break;
+
+            case 'fah' :
+            changeFahrenheit(temp);
+            break;
+
+            case 'kel' :
+            changeKelvin(temp);
+            break;
+
+            default:
+            return output = 'Please enter valid temperature type.';
+        }
+    }
+};
+//need to assign temp in global scope!@$!@£!@£12
+
+var changeCelcius = function(temp) {
+    var celToKel = temp + 273;
+    var celToFah = temp * 9 / 5 + 32;
+    console.log('test5');
+    console.log(celToKel);
+    console.log(celToFah);
+    return output = celToKel + 'K = ' + celToFah + 'F';
+};
+var changeKelvin = function(temp) {
+    var kelToCel = temp - 273;
+    var kelToFah = (temp - 273) * (9 / 5) + 32;
+    console.log(kelToCel);
+    console.log(kelToFah);
+    return output = kelToCel + 'C = ' + kelToFah + 'F';
+};
+var changeFahrenheit = function(temp) {
+    var fahToCel = (temp - 32) * (5 / 9);
+    var fahToKel = (temp - 32) * (5 / 9) + 273;
+    console.log(fahToCel);
+    console.log(fahToKel);
+    return output = fahToCel + 'C = ' + fahToKel + 'K';
+};
+
     //fahrenheit to celcius
-  var degreeCelcius = (currentInput - 32) * 5 / 9 ;
+  /*var degreeCelcius = (currentInput - 32) * 5 / 9 ;
   //fahrenheit to kelvin
   var degreeKelvin = degreeCelcius + 273 ;
   return output = currentInput + ' Fahrenheit = ' +(degreeCelcius + 'C  Or ') + (degreeKelvin + 'K');
-    }
-};
+    }*/
