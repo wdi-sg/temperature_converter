23c5671e59e3a4a1e1c589125931e4c9b526f766
Dyana Wu <dyanawu@gmail.com>
2020-03-17 17:44:56 +0800

    Complete Part 1
    - write functions f2c, f2k
    - convert input to a Number and move on if not a number

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-console.log("hello script js");
-
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+  console.log(currentInput);
+
+  var degF = Number(currentInput);
+  var inputIsNum = !isNaN(degF);
+
+  if (inputIsNum) {
+    var degC = f2c(degF).toFixed(2);
+    var degK = f2k(degF).toFixed(2);
+    var output = `${degF}F is ${degC}C, or ${degK}K.`;
+  } else {
+    var output = "I can't seem to convert that. Try again!"
+  }
+
   return output;
 };
+
+var f2c = function (degF) {
+  degC = (degF - 32) * 5 / 9;
+  return degC;
+}
+
+var f2k = function (degF) {
+  degK = (degF + 459.67) * 5 / 9;
+  return degK;
+}
