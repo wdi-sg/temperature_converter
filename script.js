b9e7f45f0d980823eb03d1eb2a1022478c405929
YusofGotboudine <yusof95@hotmail.com>
2020-03-18 08:51:04 +0800

    First Commit

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+var inputHappened = function (currentInput) {
+  if (isNaN(currentInput)) {
+    output = "please enter a numerical value!";
+  } else {
+    var celsius = (currentInput - 32) * (5 / 9);
+    var kelvin = celsius + 273.15;
+    console.log(currentInput);
+    var output = "Temp in celsius is: " + celsius + " Temp in kelvin is: " + kelvin;
+  }
   return output;
 };
