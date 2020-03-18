f27d051bd5a94b72d439855231df73edfae180df
vigbit <vigneshb@live.com>
2020-03-18 10:31:26 +0800

    part1 corrected

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("hello script js");
 
-var inputHappened = function(temperature, temperatureUnit){
+var inputHappened = function(currentInput){
   //console.log( currentInput );
   var temperature = parseInt(currentInput);
     var output = ((temperature - 32)*(5/9)+(273.15)) + "K" + " " + ((temperature - 32)*(5/9)) + "C";
