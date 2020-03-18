2c23a9479bc391f1b0a8045df3d098ed8e4105c8
dannst <arcanic@gmail.com>
2020-03-18 14:56:52 +0800

    added part2 of question

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 console.log("hello script js");
 
+// helper functions (e.g. tempKC converts K to C)
 function tempKC(temp) {
     return temp - 273.15;
 };
@@ -25,18 +26,30 @@ function tempFC(temp) {
 function tempFK(temp) {
     return tempCF(tempFC(temp));
 };
+// user input data
+var user_inputs = [];
 
+// input logic
 var inputHappened = function(currentInput){
   console.log(currentInput);
 
 console.log(typeof(currentInput) === "number")
 
-  if (typeof(parseInt(currentInput)) === "number") {
+
+
+// prompts user for temperature
+  if (!isNaN(parseInt(currentInput))) {
     var temperature = currentInput;
-    var output = "Please enter temperature unit (F, C or K)";
-  } else if (parseInt(currentInput)) === false {
+    user_inputs.push(currentInput);
+    var temp_output = "Please enter temperature unit (F, C or K)";
+  } else if (currentInput === "K") {
+    temp_output = "Temperature is currently " + user_inputs[0] + " in K, " + tempKC(user_inputs[0]) + " in C, " + tempKF(user_inputs[0]) + " in F.";
+  } else if (currentInput === "C") {
+    temp_output = "Temperature is currently " + user_inputs[0] + " in C, " + tempCK(user_inputs[0]) + " in K, " + tempCF(user_inputs[0]) + " in F.";
+  } else if (currentInput === "F") {
+    temp_output = "Temperature is currently " + user_inputs[0] + " in F, " + tempFC(user_inputs[0]) + " in C, " + tempFK(user_inputs[0]) + " in K.";
+  }
 
-}
-
+  output = temp_output
   return output;
-};
+}
