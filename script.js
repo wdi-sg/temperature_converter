c34527762c58a4a1a754578c044ca6004dcfc498
kenan <xinkenan.11@hotmail.com>
2020-03-18 09:24:21 +0800

    minor refactor

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const INVALID_INPUT_ERR_MSG = "Please enter temperature in numerical value.";
 const COLD_COMMENT = "ooh it's cold out";
 const HOT_COMMENT = "ooh it's hot out";
 const BOILING_COMMENT = "you are literally boiling";
+const CELSIUS = "C";
+const KELVIN = "K";
+const FARENHEIT = "F";
 
 const isNumericalInput = input => !isNaN(parseInt(input));
+
 const FtoC = tempInF => (tempInF - 32) * 5 / 9;
 const FtoK = tempInF => 273.5 + ((tempInF - 32) * (5/9));
 
@@ -13,7 +17,6 @@ const CtoK = tempInC => tempInC + 273.15;
 const KtoC = tempInK => tempInK - 273.15;
 const KtoF = tempInK => tempInK * 9/5 - 459.67;
 
-
 const convertTemp = (temp, formula) => formula(temp);
 const getTempComment = t => {
   if (t < 0) return COLD_COMMENT;
@@ -22,16 +25,15 @@ const getTempComment = t => {
   return "";
 }
 const getClothingRecommendation = (temp, userName) => {
-  console.log(userName);
-  let msg = `Dear ${userName},`;
-  if (temp < -20 || temp > 100 ) return msg + "dont go outside if you want to live";
-  if (temp >= -20 && temp < -10 ) return msg + "heavy jacket and toe warmers";
-  if (temp >= -10  && temp < -5 ) return msg + "heavy jacket";
-  if (temp >= -5 && temp < 10) return msg + " sweater + jacket";
-  if (temp >= 10 && temp < 20) return msg + "sweater";
-  if (temp >=20 && temp < 30) return msg + "shorts and shirt";
-  if (temp >=30 && temp < 40) return msg + "swimsuit";
-  return msg + "nothing";
+  let msg = `Dear ${userName}, `;
+  if (temp < -20 || temp > 100 ) return msg + "dont go outside if you want to live!";
+  if (temp >= -20 && temp < -10 ) return msg + "heavy jacket and toe warmers!";
+  if (temp >= -10  && temp < -5 ) return msg + "heavy jacket!";
+  if (temp >= -5 && temp < 10) return msg + " sweater + jacket!";
+  if (temp >= 10 && temp < 20) return msg + "sweater!";
+  if (temp >=20 && temp < 30) return msg + "shorts and shirt!";
+  if (temp >=30 && temp < 40) return msg + "swimsuit!";
+  return msg + "nothing!";
 }
 
 const getConvertedTemperatures = function(currentInput, tempUnit, userName) {
@@ -52,9 +54,9 @@ const getConvertedTemperatures = function(currentInput, tempUnit, userName) {
     tempInK = convertTemp(currentInput, FtoK);
 
     outTemp1 = tempInC;
-    toUnit1 = "C";
+    toUnit1 = CELSIUS;
     outTemp2 = tempInK;
-    toUnit2 = "K"
+    toUnit2 = KELVIN;
   }
 
 
@@ -64,9 +66,9 @@ const getConvertedTemperatures = function(currentInput, tempUnit, userName) {
     tempInF = convertTemp(tempInC, CtoF);
 
     outTemp1 = tempInK;
-    toUnit1 = "K";
+    toUnit1 = KELVIN;
     outTemp2 = tempInF;
-    toUnit2 = "F";
+    toUnit2 = FARENHEIT;
   }
 
 
@@ -77,9 +79,9 @@ const getConvertedTemperatures = function(currentInput, tempUnit, userName) {
 
 
     outTemp1 = tempInC;
-    toUnit1 = "C";
+    toUnit1 = CELSIUS;
     outTemp2 = tempInF;
-    toUnit2 = "F";
+    toUnit2 = FARENHEIT;
   }
 
   msg =  `${currentInput} \xB0${fromUnit} is ${outTemp1} \xB0${toUnit1} \n`;
