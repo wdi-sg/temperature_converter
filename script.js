9f3eef89e73706039e52440545f5ac6b510fba91
elisaes <alisa260785@gmail.com>
2020-06-10 00:36:29 +0800

    first part done

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 console.log("hello script js");
+const converter = function (input) {
+  const result =[];
+  const kelvinCalculation = ((input - 32) * 5) / 9 + 273.15;
+  const celciusCalculation = ((input - 32) * 5) / 9;
+  const kelvin =
+    "The temperature in kelvin is: " + kelvinCalculation.toFixed(2);
+  const celcius =
+    "The temperature in celcius is: " + celciusCalculation.toFixed(2);
+  result.push(kelvin);
+  result.push(celcius);
+  return result;
+};
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
+const inputHappened = function (currentInput) {
+   
+
+  const output = converter(currentInput);
   return output;
 };
