0db9faf36305eec2803304c52f542f2f800f87fe
metildachee <metildachee@gmail.com>
2020-06-09 15:21:23 +0800

    part 1 completed

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-console.log("hello script js");
+function appendToOutput(degree, kelvin) {
+    const output = document.getElementById('output');
+    output.innerHTML = degree + " degrees " + kelvin + " kelvin";
+}
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
-};
+function converter() {
+    // We need to grab the value from the input
+    const input = document.getElementById('input');
+    const fahrenheit = input.value;
+    const degree = parseInt(fahrenheit) - 32;
+    const kelvin = parseInt(fahrenheit) + 459;
+    appendToOutput(degree, kelvin);
+}
+
+const button = document.getElementById('button');
+button.onclick = converter;
