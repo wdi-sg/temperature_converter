4979c884f09345a188646482119411df977ae403
akjw <aseykoh@gmail.com>
2020-06-09 15:54:19 +0800

    changes input to celsius and kelvin

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+  var outputC = (currentInput - 32) * 5/9
+  var outputK = outputC + 273.15;
+  var totalOutput = currentInput + " F is " + outputC + " celsius which is " + outputK + " Kelvin."
+  return totalOutput ;
 };
