5015d7467a4716eb23ae86ba5e665071a566da77
pasti <pastitumbuck@gmail.com>
2020-06-09 21:37:33 +0800

    done

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  var output = "WOW TEMPERATURE";
-  return output;
+var convertKelvin = function(input){
+  if (parseInt(input)) {
+    var kelvin = (input - 32) + 255.372
+    console.log(kelvin)
+  } else {
+    console.log("please use a number")
+  }
+  return kelvin;
 };
+
+var convertCelcius = function(input){
+    if (parseInt(input)) {
+    var celsius = (input - 32) * 5 / 9
+    console.log(celsius)
+  } else {
+    console.log("please use a number")
+  }
+  return celsius;}
+
